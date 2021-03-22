2f7f4b47404fbe185d3d12a31906c4fdc3eb45e9
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-03-22 13:02:58 +0200

    Only "Hello World"

--- 1. Git/Hello/main.cpp
+++ 1. Git/Hello/main.cpp
@@ -1,17 +1,7 @@
-﻿#include<iostream>
+﻿#include <iostream>
 using namespace std;
-// Main method of C++ program
 int main()
 {
-	// Declare variables and set values
-	int A = 2, B = 3;
-
-	// Mathematical operation 
-	int C = A + B; 
-
-	// Print the final result of the program 
-	cout << "C=" << C << endl;
-
-	// Exit Success
-	return 0;
+    cout << "Hello World!" << endl;
+    return 0;
 }
