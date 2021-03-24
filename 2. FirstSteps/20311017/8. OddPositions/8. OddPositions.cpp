85d81e1beff827c78627aa904528a1d87ff1e734
renetatrifonova <81149331+renetatrifonova@users.noreply.github.com>
2021-03-24 21:26:19 +0200

    8. OddPositions

--- 2. FirstSteps/20311017/8. OddPositions/8. OddPositions.cpp
+++ 2. FirstSteps/20311017/8. OddPositions/8. OddPositions.cpp
@@ -5,14 +5,16 @@ using namespace std;
 int main()
 {
     int number;
-    cout << "a = " << endl;
+    cout << "Enter number :  " << endl;
     cin >> number;
   
     int a1 = (a / 10000) % 10;
-    int a2 = (a / 100) % 10;
-    int a3 = (a / 1) % 10;
+    int a2 = (a / 1000) % 10;
+    int a3 = (a / 100) % 10;
+    int a4 = (a / 10) % 10;
+    int a5 = (a / 1) % 10;
 
-    int sum = a1 + a2 + a3;
+    int sum = a1 + a3 + a5;
     cout << "Result:  " << sum << endl;
     return 0;
 }
