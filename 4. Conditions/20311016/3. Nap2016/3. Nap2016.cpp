7a0c61bdb105c259af58f912e08fb4d2705f901c
Nikolai <35627380+destbg@users.noreply.github.com>
2021-03-25 13:43:55 +0200

    20311016

---
+++ 4. Conditions/20311016/3. Nap2016/3. Nap2016.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    float income, tax;
+
+    cout << "Enter your 2016 income: ";
+    cin >> income;
+
+    if (income <= 2160) tax = 0;
+    else if (income <= 3000) tax = (income - 2160) * 0.18;
+    else if (income <= 7200) tax = 168 + ((income - 3000) * 0.20);
+    else tax = 1072 + ((income - 7200) * 0.22);
+
+    cout << "You owe: " << tax << endl;
+
+    return 0;
+}
---
+++ 4. Conditions/20311016/5. Area and perimeter/5. Area and perimeter.cpp
@@ -0,0 +1,52 @@
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int menu;
+    float p, s;
+
+    cout << "Shapes: \n";
+    cout << "1. Square \n";
+    cout << "2. Circle \n";
+    cout << "3. Triangle \n";
+    cout << "Number of shape: ";
+    cin >> menu;
+
+    if (menu == 1)
+    {
+        float a;
+        cout << "Enter side length: ";
+        cin >> a;
+        p = 4 * a;
+        s = a * a;
+    }
+    else if (menu == 2)
+    {
+        float r;
+        cout << "Enter circle radius: ";
+        cin >> r;
+        p = 2 * M_PI * r;
+        s = M_PI * pow(r, 2);
+    }
+    else if (menu == 3)
+    {
+        float a, b, c;
+        cout << "Enter first side lenght: ";
+        cin >> a;
+        cout << "Enter second side lenght: ";
+        cin >> b;
+        cout << "Enter third side lenght: ";
+        cin >> c;
+        p = a + b + c;
+        float p2 = p / 2;
+        s = sqrt(p2 * (p2 - a) * (p2 - b) * (p2 - c));
+    }
+
+    cout << "Perimeter = " << p << endl;
+    cout << "Area = " << s << endl;
+
+    return 0;
+}
