ae6c11465b2e0b37ebf5fbdd445f4fb3c179b1b1
Nikolai <35627380+destbg@users.noreply.github.com>
2021-03-25 09:05:56 +0200

    20311016

---
+++ 2. FirstSteps/20311016/2. Sum/2. Sum.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+
+    cout << "a=";
+    cin >> a;
+
+    cout << "b=";
+    cin >> b;
+
+    c = a + b;
+
+    cout << "Sum=" << c << endl;
+
+    return 0;
+  
+}
+
