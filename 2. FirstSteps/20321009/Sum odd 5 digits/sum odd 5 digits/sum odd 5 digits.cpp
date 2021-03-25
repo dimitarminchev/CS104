d3dc58dcfdf86fbba4b6404b7fdfa1cbe8d311a7
Максим Димитров <20321009@students.bfu.bg>
2021-03-25 00:31:17 -0700

    Update

---
+++ 2. FirstSteps/20321009/Sum odd 5 digits/sum odd 5 digits/sum odd 5 digits.cpp
@@ -0,0 +1,16 @@
+﻿// Сума от цифрите на нечетни позиции на 5 цифрено число
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "a=?" << endl;
+    cin >> a;
+    int a1 = (a / 10000) % 10;
+    int a3 = (a / 100) % 10;
+    int a5 = (a / 1) % 10;
+    int s = a1 + a3 + a5;
+    cout << "Sum: " << s << endl;
+    return 0;
+}
