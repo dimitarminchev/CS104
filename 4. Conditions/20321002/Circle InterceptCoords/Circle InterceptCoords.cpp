fc547e81ca46eb3e93af69d8d2ec895b8ed35d6b
Илиян Пондалов <20321002@students.bfu.bg>
2021-03-25 14:06:31 +0200

    Day 3

--- 4. Conditions/20321002/Circle InterceptCoords/Circle InterceptCoords.cpp
+++ 4. Conditions/20321002/Circle InterceptCoords/Circle InterceptCoords.cpp
@@ -3,31 +3,35 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
 int main()
 {
-    float Ax, Ay, Ar, Bx, By, Br //a, b, h, Tx, Ty, Cx, Cy, Dx, Dy;
+    float Ax, Ay, Ar, Bx, By, Br; //a, b, h, Tx, Ty, Cx, Cy, Dx, Dy;
 
     cout << "Enter first circle [x y r]:" << endl;
     cin >> Ax >> Ay >> Ar;
     cout << "Enter second circle [x y r]:" << endl;
     cin >> Bx >> By >> Br;
 
-    float d = sqrt(pow((Ax - Bx), 2) + pow((Ay - By), 2));
+    double d = sqrt(pow((Ax - Bx), 2) + pow((Ay - By), 2));
 
     if (Ar + Br < d) cout << "No interception points." << endl;
     if (Ar + Br == d) cout << "One interception point." << endl;
-    if (Ar + Br > d) cout << "Two interception points."
+    if (Ar + Br > d) cout << "Two interception points." << endl;
+
     {
-        float b = (pow(Br, 2) - pow(Ar, 2) + pow(d, 2));
-        float a = d - b;
-        float h = sqrt(pow(Br, 2) - pow(b, 2);
-        float Tx = Ax + a / d * (Bx - Ax);
-        float Ty = Ay + a / d * (By - Ay);
-        float Cx = Tx + ((By - Ay) / d) * h;
-        float Cy = Ty - ((Bx - Ax) / d) * h;
-        float Dx = Tx - ((By - Ay) / d) * h;
-        float Dy = Ty + ((Bx - Ax) / d) * h;
-        cout << "Cx:Cy="; cout << Cx << cout << Cy << endl;
+        double b = (pow(Br, 2) - pow(Ar, 2) + pow(d, 2));
+        double a = d - b;
+        double h = sqrt(pow(Br, 2) - pow(b, 2));
+        double Tx = Ax + a / d * (Bx - Ax);
+        double Ty = Ay + a / d * (By - Ay);
+        double Cx = Tx + ((By - Ay) / d) * h;
+        double Cy = Ty - ((Bx - Ax) / d) * h;
+        double Dx = Tx - ((By - Ay) / d) * h;
+        double Dy = Ty + ((Bx - Ax) / d) * h;
+        cout << "Cx:Cy=" << Cx << Cy << endl;
+
+
     }
 
     return 0;
