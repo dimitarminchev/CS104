e523a1344194d9a669e9140ca66effd04d31497a
Максим Димитров <20321009@students.bfu.bg>
2021-03-25 09:17:26 -0700

    Update

--- 2. FirstSteps/20321009/Sum even 6 digits/sum even 6 digits/sum even 6 digits.cpp
+++ 2. FirstSteps/20321009/Sum even 6 digits/sum even 6 digits/sum even 6 digits.cpp
@@ -8,19 +8,16 @@ int main()
     int a;
 
     // Въвеждане на 6 цифрено число
-    cout << "a = ?" << endl;
+    cout << "a = " << endl;
     cin >> a;
 
     // Изчисления
-    int a1 = (a / 100000) % 10;
-    int a2 = (a / 10000) % 10;
-    int a3 = (a / 1000) % 10;
-    int a4 = (a / 100) % 10;
-    int a5 = (a / 10) % 10;
-    int a6 = (a / 1) % 10;
+    int a6 = (a / 100000) % 10;
+    int a4 = (a / 1000) % 10;
+    int a2 = (a / 10) % 10;
 
     // Резултат
-    int b = a2 + a4 + a6;
+    int b = a6 * a4 * a2;
     cout << "sum = " << b << endl;
 
     return 0; 
