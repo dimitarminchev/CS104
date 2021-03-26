48d5c0aaa66e63bb92f0d2a3ae19514c0d901319
Мария Райкова <20321029@students.bfu.bg>
2021-03-26 19:54:53 +0200

    20321029

---
+++ 5. Cycles/20321029/Sum/Sum.cpp
@@ -0,0 +1,25 @@
+﻿
+// Сумата на естествените числа до зададено число
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    int n, sum = 0;
+
+    
+    cout << "n=";
+    cin >> n;
+
+    
+    for (int i = 1; i <= n; i++)
+    {
+        
+        sum = sum + i;
+    }
+
+    
+    cout << "sum=" << sum << " ";
+
+    return 0;
+}
