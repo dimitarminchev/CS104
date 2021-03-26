04612d806418439381c313439d187112bde70451
mitko17 <mitkodim739@gmail.com>
2021-03-26 19:08:56 +0200

    Added new project (Day4/Cycles)

---
+++ 5. Cycles/20311014/10. PascalTriangle/10. PascalTriangle.cpp
@@ -0,0 +1,27 @@
+﻿// 10. Отпечатайте триъгълника на Паскал
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int size, n = 1;
+    cout << "Enter triangle size: ";
+    cin >> size;
+    cout << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int row = 1; row <= size - i; row++) cout << "  ";
+        cout << "1   ";
+
+        for (int j = 1; j <= i; j++)
+        {
+            n = n * (1 + i - j) / j;
+            if (n <= 9) cout << n << "   ";
+            else if (n >= 10) cout << n << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
