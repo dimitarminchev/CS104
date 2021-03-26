78e3fa558a86b8d960ff7d4a231435250642e8bc
Georgi Belov <banizawq@gmail.com>
2021-03-26 13:35:07 +0200

    20321034GeorgiBelov

---
+++ 2. FirstSteps/20321034/20321034/20321034.cpp
@@ -0,0 +1,8 @@
+//01.Здравей свят!
+#include <iostream>
+using namespace std;
+int  main()
+{
+    cout << "Hello World!" << endl;
+    return
+    }
---
+++ 2. FirstSteps/20321034/3.Reactangle/3.Reactangle.cpp
@@ -0,0 +1,20 @@
+// 3.Периметър и лице на правоъгълник.
+#include <iostream>
+
+using namespace std;
+int  main()
+{
+	float a, b, p, s;
+	cout << "a=";
+		cin >> a;
+
+	cout << "b=";
+	cin >> b;
+
+	p = 2 * (a + b);
+	s = a * b;
+
+	cout << "p=" << p << endl;
+	cout << "s=" << s << endl;
+
+}
