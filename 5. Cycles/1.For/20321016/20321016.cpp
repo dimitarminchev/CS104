6c68844dc36147a4a8514292b1c8efec7b07dd52
zafirmanev <20321016@students.bfu.bg>
2021-03-29 11:16:51 +0300

    20321016

---
+++ 5. Cycles/1.For/20321016/20321016.cpp
@@ -0,0 +1,17 @@
+// 20321016.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 0;
+
+    // Цикличен алгоритъм с повторения
+    for (i = 1; i < 100; i++)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
