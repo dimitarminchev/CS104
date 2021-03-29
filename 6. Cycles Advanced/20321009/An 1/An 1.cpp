3565d260ce82e50f5eabd652772a0b09071dc8c6
Максим Димитров <20321009@students.bfu.bg>
2021-03-29 10:54:17 -0700

    Day 5

--- 6. Cycles Advanced/20321009/An 1/An 1.cpp
+++ 6. Cycles Advanced/20321009/An 1/An 1.cpp
@@ -1,20 +1,22 @@
-// An 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿// 4. Изведете редицата: Аn = 1/n
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Променливи
+    int n;
+
+    // Въвеждане на горна граница
+    cout << "Upper Limit: ";
+    cin >> n;
+
+    // Изчисления 
+    for (int a = 1; a <= n; a++)
+    {
+        float Ab = 1.0 / a;
+        cout << Ab << endl;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
