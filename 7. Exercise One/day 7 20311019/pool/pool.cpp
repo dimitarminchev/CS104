f507cfb590fd4e21624651d89569e0855a785295
Slavi-7 <81153620+Slavi-7@users.noreply.github.com>
2021-04-02 10:43:24 +0300

    Add files via upload

---
+++ 7. Exercise One/day 7 20311019/pool/pool.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a, b, c;
+    int tubes;
+    cout << "Enter the dimentions of the pool: " << endl;
+    cout << "a: ";
+    cin >> a;
+    cout << "b: " ;
+    cin >> b;
+    cout << "c: ";
+    cin >> c;
+    int d1, d2, d3;
+    cout << "d1(in cubic meters per hour): ";
+    cin >> d1;
+    cout << "d2(in cubic meters per hour): ";
+    cin >> d2;
+    cout << "d3(in cubic meters per hour): ";
+    cin >> d3;
+
+    double V = a * b * c;
+    double D = d1 + d2 + d3;
+    double Time = V / D;
+    cout << "The pool will be full in " << Time << " hours.";
+
+    return 0;
+}
