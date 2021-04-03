2b43ebd5de4192f6f2766e267c86976eb04b0160
Максим Димитров <20321009@students.bfu.bg>
2021-04-03 06:13:24 -0700

    HW 2

---
+++ 7. Exercise One/20321009/Swimming pool/Swimming pool.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    // Променливи 
+    float a, b, c;
+    int D1, D2, D3;
+
+    
+    // Въвеждане 
+    cout << "Enter pool's dimensions : " << endl;
+   
+    cout << "a = ";
+    cin >> a;
+   
+    cout << "b = ";
+    cin >> b;
+   
+    cout << "c = ";
+    cin >> c;
+
+    cout << endl;
+
+    
+    // Въвеждане на куб. метри за час 
+    cout << "Enter the cubic meters per hour: " << endl;
+
+    cout << "D1 = ";
+    cin >> D1;
+
+    cout << "D2 = ";
+    cin >> D2;
+
+    cout << "D3 = ";
+    cin >> D3;
+
+    cout << endl;
+
+    
+    // Изчисления 
+    double B = a * b * c;
+    double D = D1 + D2 + D3;
+    double T = B / D;
+    cout << " The pool will be full in: " << T << " hours :) " << endl ;
+    
+   return 0;
+}
