4205adf08244c9e439542853fd2df80ab1e1f664
rizzoff <rizzoff@mail.ru>
2021-04-04 15:52:48 +0300

    Day 5 tasks

---
+++ 6. Cycles Advanced/20311029/04.An1/04.An1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    float ak;
+
+    cout << "Upper Limit: ";
+    cin >> n;
+
+    for (int k = 1; k <= n; k++)
+    {
+        ak = 1.0 / k;
+        cout << ak << endl;
+    }
+
+    return 0;
+}
---
+++ 6. Cycles Advanced/20311029/05.An2/05.An2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    float ak;
+
+    cout << "Upper Limit: ";
+    cin >> n;
+
+    for (int k = 1; k <= n; k++)
+    {
+        ak = 1.0 / pow(k, 2);
+        cout << ak << endl;
+    }
+
+    return 0;
+}
