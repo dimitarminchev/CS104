0ff4460231060c3de5cdf92919fc5b374f9c1819
Максим Димитров <20321009@students.bfu.bg>
2021-04-07 03:29:09 -0700

    Ahh

---
+++ 8. Test One/20321009/I/I.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+
+	for (int i = a; i <= b; i++)
+	{
+		if (i % 2 == 0)
+		{
+			cout << i <<" ";
+		}
+	}
+
+	return 0;
+}
