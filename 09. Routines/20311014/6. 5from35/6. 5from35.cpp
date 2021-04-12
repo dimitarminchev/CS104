88316d808a001e419a3c87e42b9277b6cac34964
mitko17 <mitkodim739@gmail.com>
2021-04-12 11:33:20 +0300

    Fixed typo

--- 09. Routines/20311014/6. 5from35/6. 5from35.cpp
+++ 09. Routines/20311014/6. 5from35/6. 5from35.cpp
@@ -13,10 +13,10 @@ int main()
     cout << "1) 5/35 = 1 in " << C_5_35 << endl;
 
     int C_6_42 = fak(42) / (fak(6) * fak(42 - 6));
-    cout << "1) 6/42 = 1 in " << C_6_42 << endl;
+    cout << "2) 6/42 = 1 in " << C_6_42 << endl;
 
     int C_6_49 = fak(49) / (fak(6) * fak(49 - 6));
-    cout << "1) 6/49 = 1 in " << C_6_49 << endl;
+    cout << "3) 6/49 = 1 in " << C_6_49 << endl;
 
     return 0;
 }
