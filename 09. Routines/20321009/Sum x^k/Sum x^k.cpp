ead614ef57d7a019771cdc342e342ab1264eb497
Максим Димитров <20321009@students.bfu.bg>
2021-04-16 09:47:09 -0700

    20321009

---
+++ 09. Routines/20321009/Sum x^k/Sum x^k.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+/// <summary>
+/// Сума на x^k
+/// </summary>
+/// <param name="a">Параметър x</param>
+/// <param name="b">Параметър n</param>
+/// <returns>Сума</returns>
+int f(int x = 1, int n = 1)
+{
+
+	int sum = 0;
+	for (int k = 1; k <= n; k++)
+	{
+		sum = sum + pow(x, k);
+	}
+	return sum;
+}
+
+/// <summary>
+/// Главна функция на програмата
+/// </summary>
+int main()
+{
+	cout << "The sum is: " << f(2, 3) << endl;
+	
+	return 0;
+}
+
