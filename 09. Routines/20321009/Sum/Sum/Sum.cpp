a738d5c67fbab8ed4f8bb020aeeefba12a879523
Максим Димитров <20321009@students.bfu.bg>
2021-04-16 01:59:58 -0700

    20321009

---
+++ 09. Routines/20321009/Sum/Sum/Sum.cpp
@@ -0,0 +1,24 @@
+﻿// Функция за сумиране на две числа
+#include <iostream>
+using namespace std;
+
+/// <summary>
+/// Функция за сумиране на две числа
+/// </summary>
+/// <param name="a">- First number</param>
+/// <param name="b">- Second number</param>
+/// <returns></returns>
+int sum(int a = 2, int b = 3)
+{
+    return (a + b);
+}
+
+
+int main()
+{
+    cout << sum() << endl;    // 5
+    cout << sum(1) << endl;   // 4
+    cout << sum(1, 2) << endl;// 3
+    return 0;
+}
+
