e5e56bc05c6ac09b46808b61f73c188bc9f6a27c
Тодор Купенов <20311022@students.bfu.bg>
2021-04-19 11:40:51 +0300

    a

---
+++ 05. Cycles/20311022/2.While/2.While.cpp
@@ -0,0 +1,16 @@
+﻿// Упражнение 2 - цикъл с предусловие 
+#include <iostream>
+using namespace std;
+int main()
+{
+
+	int i = 1;
+
+	while (i <= 100)
+	{
+		cout << i << ' ';
+		i++;
+	}
+
+	return 0;
+}
