f357f54dc466ebc85592860033d5741f5a2daf68
Виолетта Мартинова <20311006@students.bfu.bg>
2021-05-10 09:10:51 +0300

    11. Matrixes

--- 11. Matrixes/20311006/4. Adamar Product/4. Adamar Product.cpp
+++ 11. Matrixes/20311006/4. Adamar Product/4. Adamar Product.cpp
@@ -2,10 +2,7 @@
 using namespace std;
 int main()
 {
-    // Деклариране на три двумерни масива (Матрици)
     int A[3][3], B[3][3], C[3][3];
-
-    // Въвеждане на елементите на матрицата А
     cout << "Please enter first matrix elements:" << endl;
     for (int x = 0; x < 3; x++)
     {
@@ -14,8 +11,6 @@ int main()
             cin >> A[x][y];
         }
     }
-
-    // Въвеждане на елементите на матрицата B
     cout << "Please enter second matrix elements:" << endl;
     for (int x = 0; x < 3; x++)
     {
@@ -24,17 +19,15 @@ int main()
             cin >> B[x][y];
         }
     }
-
-    // Отпечатване на сума
     cout << "Matrix Adamar Product: " << endl;
     for (int x = 0; x < 3; x++)
     {
         for (int y = 0; y < 3; y++)
         {
-            C[x][y] = A[x][y] * B[x][y]; // Поелементно умножение
-            cout << C[x][y] << '\t'; // Tab
+            C[x][y] = A[x][y] * B[x][y];
+            cout << C[x][y] << '\t';
         }
-        cout << '\n'; // cout << endl;
+        cout << '\n';
     }
 
     return 0;
