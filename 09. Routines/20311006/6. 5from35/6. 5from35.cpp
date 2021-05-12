c750e0bb4531da077a7399ab960ebd7200203bdb
Виолетта Мартинова <20311006@students.bfu.bg>
2021-05-12 21:36:14 +0300

    Routines tasks

--- 09. Routines/20311006/6. 5from35/6. 5from35.cpp
+++ 09. Routines/20311006/6. 5from35/6. 5from35.cpp
@@ -1,20 +1,11 @@
 ﻿#include <iostream>
 using namespace std;
-
-/// <summary>
-/// Рекурсивна функция за намиране на Факториел
-/// </summary>
-/// <param name="n">Търсен фекториел</param>
-/// <returns>Резултат</returns>
 double fak(int n)
 {
-    if (n <= 1) return 1; // exit 
+    if (n <= 1) return 1;
     else return n * fak(n - 1);
 }
 
-/// <summary>
-/// Главна функция на програмата
-/// </summary>
 int main()
 {
     double C_5_35 = fak(35) / (fak(5) * fak(35 - 5));
