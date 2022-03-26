31d08bc9088471d35f0f35c4137291814ac9bc0c
siimonaa <simona123510@abv.bg>
2022-03-26 14:56:07 +0200

    First tasks

---
+++ 2022/02. FirstSteps/21311041/7. SumOddNum/7. SumOddNum.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "a=?" << endl;
+    cin >> a; // 12345
+
+    int a1 = (a / 10000) % 10; // 1
+    int a2 = (a / 1000) % 10; // 2
+    int a3 = (a / 100) % 10; // 3
+    int a4 = (a / 10) % 10; // 4
+    int a5 = a % 10; //5
+
+    int s = a1 + a3 + a5;
+
+    cout << s << endl;
+
+    return 0;
+}
