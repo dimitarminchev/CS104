3979bd65e37e547505de9e4552f4c9bcb3357cd5
Qsen02 <diyangenchev73@gmail.com>
2022-04-02 23:38:08 +0300

    готово

---
+++ 2022/05. Cycles/21311017/Do while/Do while/Do while.cpp
@@ -0,0 +1,15 @@
+ #include <iostream>
+using namespace std;
+int main()
+{
+    int i = 1;
+
+    // Цикличен алгоритъм с постусловие
+    do
+    {
+        cout << i << " ";
+        i++;
+    } while (i <= 100);
+
+    return 0;
+}
