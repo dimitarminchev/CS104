ca2a8dfc37850a6c373cd31ea2a457c13a00054c
Viktor <viktor.kirov139@gmail.com>
2022-05-23 23:29:27 +0300

    hello, sum, rectangle, circle, reverse number, sumpro, зад 8, зад 9

---
+++ 2022/02. FirstSteps/21311014/Sum/Sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+
+	cout << "a,b=?" << endl;
+
+	cin >> a >> b;
+
+	int c = a + b;
+	cout << "a+b=" << c << endl;
+
+	return 0;
+}
