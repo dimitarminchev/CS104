e55c6c67b9c38338b68f7ed2d98618e377927f26
35989 <35989@LAPTOP-N6Q8M1AE>
2022-05-25 14:56:41 +0300

    Done

---
+++ 2022/01. Git/21321013/Sum/Sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    a = 12;
+    b = 23;
+    c = a + b;
+
+    cout << "Sum: " << c;
+
+    return 0;
+}
---
+++ 2022/03. Maths/Задача 5/Задача 5/Задача 5.cpp
@@ -0,0 +1,20 @@
+﻿// Задача 5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	float a, b;
+	cout << "b1=?, b2=?" << endl;
+	cin >> a >> b;
+
+	int b3 = (a + b) / 3;
+	int b1 = b3 - a;
+	int b2 = b3 - b;
+	cout << "b1:" << b1 << " b2:" << b2 << " b3: " << b3 << endl;
+
+	return 0;
+}
+
+
