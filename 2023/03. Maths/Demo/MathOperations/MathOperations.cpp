67094e11d59e5599dba8b1e5fd9ab2a233883e0e
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-03-26 18:02:15 +0300

    26.03.2022

---
+++ 2023/03. Maths/Demo/MathOperations/MathOperations.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+
+    cout << "a="; 
+    cin >> a;
+
+    cout << "b=";
+    cin >> b;
+
+    cout << (a + b) << endl; 
+    cout << (a - b) << endl; 
+    cout << (a * b) << endl; 
+    cout << (a / b) << endl; 
+    cout << (a % b) << endl; 
+
+    return 0;
+}
