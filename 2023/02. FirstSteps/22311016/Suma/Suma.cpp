847edc717d35b270368160c7c160e368d4c57c97
Ted-Teodor Stoyanov <ted03teodorssaq1202@gmail.com>
2023-03-28 21:09:44 +0300

    Suma, ReverseNumber

---
+++ 2023/02. FirstSteps/22311016/Suma/Suma.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+
+{
+
+    int a, b;
+    cout << "Please enter a number for a = ";
+    cin >> a;
+    cout << "Please enter a number for b = ";
+    cin >> b;
+    int c = a + b;
+    cout << "a + b = " << c << endl;
+    return 0;
+
+}
