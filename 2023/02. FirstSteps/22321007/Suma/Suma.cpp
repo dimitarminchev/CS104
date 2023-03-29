9fb9150daa37894ae3eb0a25970b469df0e3df8f
Pavlin Slavov <bathala@abv.bg>
2023-03-29 20:56:16 +0300

    Completed

---
+++ 2023/02. FirstSteps/22321007/Suma/Suma.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+
+    int a, b;
+    cout << "Please enter a number for a = ";
+    cin >> a;
+    cout << "Please enter a number for b = ";
+    cin >> b;
+    int c = a + b;
+    cout << "a + b = " << c << endl;
+    return 0;
+
+}
