8c6074620009eb070a9d428a3f3f9757db93acd0
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-03-30 17:39:48 +0300

    30.03.2023

---
+++ 2023/06. Routines/Demo/RecursiveFactoriel/RecursiveFactoriel.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+// Рекурсивна функция за намиране на факториел
+double fak(int n)
+{
+    if (n == 1) return 1;
+    else return n * fak(n - 1);
+}
+
+// Главна функция
+int main()
+{
+    double C = fak(49) / (fak(6) * fak(49 - 6));
+    cout << C << endl;
+    return 0;
+}
