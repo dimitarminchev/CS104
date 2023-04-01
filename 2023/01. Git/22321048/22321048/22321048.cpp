812868fd33751759d0f5f7a7329fb12933fe43c7
D-Stoqnov <dimitarstoqnov22@gmail.com>
2023-03-31 21:30:20 -0700

    22321048

--- 2023/01. Git/22321048/22321048/22321048.cpp
+++ 2023/01. Git/22321048/22321048/22321048.cpp
@@ -4,13 +4,11 @@ using namespace std;
 int main()
 {
 
-#include <iostream>
-    using namespace std;
-    int main();
-    {
-         cout << "Hello" << endl;
-        
-         return 0;
-    }
+
+ 
+        cout << "Hello ";
+
+
+        return 0; 
 }
 
