6537732518fef597fc1b6e411fcd994d9a05e215
Стивън Иванов <22321029@students.bfu.bg>
2023-04-03 11:45:03 +0300

    22321029

---
+++ 2023/01. Git/22321029/22321029/22321029/22321029.cpp
@@ -0,0 +1,11 @@
+// 22321029.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
