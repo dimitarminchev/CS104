c6fa5ac31c3b1c652af1c7b58ff71c8a18f4ffce
tediyaneva <teodora.yaneva2003@gmail.com>
2023-04-06 00:25:43 +0300

    Done

---
+++ 2023/06. Routines/22311026/RecursiveStep2/RecursiveStep2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int step2(int n)
+{
+    if (n == 1) return 2;
+    else return 2 * step2(n - 1);
+}
+
+int main()
+{
+    cout << step2(10) << endl;
+    return 0;
+}
