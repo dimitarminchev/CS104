f180fcc891746fe8e3922e3001b4247a02d74099
hristian333 <bonev_hristian@abv.bg>
2023-04-10 00:26:31 +0300

    Done!

---
+++ 2023/07. Arrays/22311017/ArrayInputOutput/ArrayInputOutput.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<iostream>
+using namespace std;
+int main()
+{
+    const int N = 10;
+
+    
+    int A[N];
+
+    
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << " ";
+    }
+    return 0;
+}
