a37190aa200ae86b6d704ab135536ed7dc605207
Ivan Kostov <ivan.d.kostov@gmail.com>
2023-04-23 19:52:03 +0300

    Done

---
+++ 2023/03. Maths/22311042/SurfaceAndAreaOfRectangle/SurfaceAndAreaOfRectangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a, b;
+
+    cout << "a=";
+    cin >> a;
+
+    cout << "b=";
+    cin >> b;
+
+    float p = 2 * (a + b);
+    float s = a * b;
+
+    cout << "p=" << p << endl;
+    cout << "s=" << s << endl;
+
+    return 0;
+}
