63f884b1577390ea02bcf01098c9a84d67c1311b
Румяна Каракушева <22321011@students.bfu.bg>
2023-04-27 16:40:55 +0300

    .

--- 2023/03. Maths/22321011/SurfaceAndAreaOfCircle/SurfaceAndAreaOfCircle/SurfaceAndAreaOfCircle.cpp
+++ 2023/03. Maths/22321011/SurfaceAndAreaOfCircle/SurfaceAndAreaOfCircle/SurfaceAndAreaOfCircle.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    // Периметър и лице на кръг
+    float r;
+
+    cout << "r=";
+    cin >> r;
+
+    double pi = 2 * acos(0.0);
+    float p = 2 * pi * r;
+    float s = pi * pow(r, 2);
 
     cout << "p=" << p << endl;
     cout << "s=" << s << endl;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    return 0;
+}
