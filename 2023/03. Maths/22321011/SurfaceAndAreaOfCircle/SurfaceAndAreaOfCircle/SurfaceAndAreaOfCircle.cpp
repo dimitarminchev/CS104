86784a963258469301e9e3a0072ccd188db581da
Румяна Каракушева <22321011@students.bfu.bg>
2023-04-27 16:23:34 +0300

    !

--- 2023/03. Maths/22321011/SurfaceAndAreaOfCircle/SurfaceAndAreaOfCircle/SurfaceAndAreaOfCircle.cpp
+++ 2023/03. Maths/22321011/SurfaceAndAreaOfCircle/SurfaceAndAreaOfCircle/SurfaceAndAreaOfCircle.cpp
@@ -1,20 +1,27 @@
-// SurfaceAndAreaOfCircle.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
+#include <iostream> // cout, cin
+#include <cmath> // acos, pow
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    float r;
+
+    cout << "r=";
+    cin >> r;
+
+    double pi = 2 * acos(0.0);
+    float p = 2 * pi * r;
+<<<<<<< HEAD
+    float s = pi * pow(r, 2);
+=======
+    float s = pi * pow(r,2);
+>>>>>>> b2123fa79974ab214be7e5272432c38eab1b3548
+
+    cout << "p=" << p << endl;
+    cout << "s=" << s << endl;
+
+    return 0;
+<<<<<<< HEAD
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+=======
+}
+>>>>>>> b2123fa79974ab214be7e5272432c38eab1b3548
