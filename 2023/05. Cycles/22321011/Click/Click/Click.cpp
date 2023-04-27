62b49e330eb4a11febb3171890d0d7c9c65e3a65
Румяна Каракушева <22321011@students.bfu.bg>
2023-04-27 17:12:31 +0300

    .

--- 2023/05. Cycles/22321011/Click/Click/Click.cpp
+++ 2023/05. Cycles/22321011/Click/Click/Click.cpp
@@ -1,20 +1,28 @@
-// Click.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Game "click"
+    int k;
+
+    // Let's play to 1000
+    for (k = 1; k <= 1000; k++)
+    {
+        // Splitting the digits of the number
+        int k3 = (k / 100) % 10;
+        int k2 = (k / 10) % 10;
+        int k1 = (k / 1) % 10;
+
+        // Checking if any of these digits is not 7
+        if (k1 == 7 || k2 == 7 || k3 == 7 || k % 7 == 0)
+        {
+            cout << "click ";
+        }
+        else
+        {
+            cout << k << " ";
+        }
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
