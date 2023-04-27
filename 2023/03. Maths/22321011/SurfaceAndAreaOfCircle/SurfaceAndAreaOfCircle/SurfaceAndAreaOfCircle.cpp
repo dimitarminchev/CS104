88a52c1ee36840311718f1cfc1d510e24f84e9b6
Румяна Каракушева <22321011@students.bfu.bg>
2023-04-27 16:26:04 +0300

    .

--- 2023/03. Maths/22321011/SurfaceAndAreaOfCircle/SurfaceAndAreaOfCircle/SurfaceAndAreaOfCircle.cpp
+++ 2023/03. Maths/22321011/SurfaceAndAreaOfCircle/SurfaceAndAreaOfCircle/SurfaceAndAreaOfCircle.cpp
@@ -10,18 +10,10 @@ int main()
 
     double pi = 2 * acos(0.0);
     float p = 2 * pi * r;
-<<<<<<< HEAD
     float s = pi * pow(r, 2);
-=======
-    float s = pi * pow(r,2);
->>>>>>> b2123fa79974ab214be7e5272432c38eab1b3548
 
     cout << "p=" << p << endl;
     cout << "s=" << s << endl;
 
     return 0;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> b2123fa79974ab214be7e5272432c38eab1b3548
