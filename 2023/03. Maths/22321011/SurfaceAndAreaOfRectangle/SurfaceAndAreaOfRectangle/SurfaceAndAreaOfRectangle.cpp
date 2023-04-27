356a14887f499002e9436342186c7681854fe353
Румяна Каракушева <22321011@students.bfu.bg>
2023-04-27 16:44:08 +0300

    .

--- 2023/03. Maths/22321011/SurfaceAndAreaOfRectangle/SurfaceAndAreaOfRectangle/SurfaceAndAreaOfRectangle.cpp
+++ 2023/03. Maths/22321011/SurfaceAndAreaOfRectangle/SurfaceAndAreaOfRectangle/SurfaceAndAreaOfRectangle.cpp
@@ -1,20 +1,21 @@
-// SurfaceAndAreaOfRectangle.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Периметър и лице на правоъгълник
+    float a, b;
+
+    cout << "a=";
+    cin >> a;
+
+    cout << "b=";
+    cin >> b;
+
+    float p = 2 * (a + b);
+    float s = a * b;
+
+    cout << "p=" << p << endl;
+    cout << "s=" << s << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
