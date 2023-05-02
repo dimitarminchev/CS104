51406066d388f4e80f2dd2864486daed60d5dadf
DenislavBelev <128911691+DenislavBelev@users.noreply.github.com>
2023-05-02 15:55:29 +0300

    Add files via upload

---
+++ 2023/04. Conditions/22311025 maxoftwonumbers/22311025 maxoftwonumbers.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a, b;
+
+    cout << "a=";
+    cin >> a;
+
+    cout << "b=";
+    cin >> b;
+
+    if (a > b)
+    {
+        cout << "a>b" << endl;
+    }
+    else
+    {
+        cout << "b>a" << endl;
+    }
+
+    return 0;
+}
