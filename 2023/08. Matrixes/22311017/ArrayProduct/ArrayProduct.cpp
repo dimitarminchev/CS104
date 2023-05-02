d21f4f7f9d5bbb6ba38497432a5d5af30e1d9aeb
hristian333 <bonev_hristian@abv.bg>
2023-05-02 09:29:34 +0300

    Done!

--- 2023/08. Matrixes/22311017/ArrayProduct/ArrayProduct.cpp
+++ 2023/08. Matrixes/22311017/ArrayProduct/ArrayProduct.cpp
@@ -1,20 +1,57 @@
-// ArrayProduct.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+
+const int X = 3, Y = 3;
+
+
+void Input(int A[X][Y])
+{
+    cout << "Enter the matrix numbers:\n";
+    for (int x = 0; x < X; x++)
+    {
+        for (int y = 0; y < Y; y++)
+        {
+            cin >> A[x][y];
+        }
+    }
+}
+
+
+void Print(int A[X][Y])
+{
+    for (int x = 0; x < X; x++)
+    {
+        for (int y = 0; y < Y; y++)
+        {
+            cout << A[x][y] << '\t';
+        }
+        cout << '\n';
+    }
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    int A[X][Y], B[X][Y], C[X][Y], n;
+
+    
+    Input(A);
+    Input(B);
+
+    
+    for (int x = 0; x < X; x++)
+    {
+        for (int y = 0; y < Y; y++)
+        {
+            C[x][y] = A[x][0] * B[0][y] + A[x][1] * B[1][y] + A[x][2] * B[2][y];
+        }
+    }
+
+    
+    cout << "The result is amazing:\n";
+    Print(C);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
