2bee0faa3739082cd0365545e8f74c3dd8c6e03b
IlinaKotseva <ilina.kotseva@gmail.com>
2023-05-08 02:18:53 +0300

    done

---
+++ 2023/04. Conditions/22311038/MaxOtThreeNumbers/MaxOtThreeNumbers.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+
+	cout << "Enter a,b and c: ";
+	cin >> a >> b >> c;
+
+	int max = a;
+	if (b > max) max = b;
+	if (c > max) max = c;
+
+	cout << "max=" << max << endl;
+
+	return 0;
+}
