f025d2908ccbae8f25e1d4a1eaddb3baca9dc2df
Лора Михаилова <lora@alein.org>
2023-05-10 20:24:25 +0300

    done

--- 2023/06. Routines/22321004/RecursiveStep2/RecursiveStep2.cpp
+++ 2023/06. Routines/22321004/RecursiveStep2/RecursiveStep2.cpp
@@ -1,20 +1,14 @@
-// RecursiveStep2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+int step2(int n)
+{
+    if (n == 1) return 2;
+    else return 2 * step2(n - 1);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << step2(10) << endl;
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
