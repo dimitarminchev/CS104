5bc7a5a972ed2bf88b570613583c366a4f095b65
Kristian Mladenov <krstntank@gmail.com>
2023-05-10 23:32:16 +0300

    Done

---
+++ 2023/04. Conditions/22311027/MaxOtThreeNumbers.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+
+	cout << "a = ";
+	cin >> a;
+
+	cout << "b = ";
+	cin >> b;
+
+	cout << "c = ";
+	cin >> c;
+
+	int max = a;
+
+	if (b > max)
+	{
+		max = b;
+	}
+
+	if (c > max)
+	{
+		max = c;
+	}
+
+	cout << "max = " << max << endl;
+
+	return 0;
+}
