395f2151e63d095179ead02b9abc0058bdc5597c
Mehmed Nedjatin <teryeriblack@gmail.com>
2023-05-10 23:34:36 +0300

    Done

--- 2023/07. Arrays/22311028/ArraySum/ArraySum.cpp
+++ 2023/07. Arrays/22311028/ArraySum/ArraySum.cpp
@@ -1,20 +1,25 @@
-// ArraySum.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
+﻿﻿#include<iostream>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int N = 100;
+
+    // Деклариране на масив с N елемента
+    double A[N];
+
+    // Попълваме масива с числа
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = i + 1;
+    }
+
+    // Намиране на сума
+    double sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        sum += A[i];
+    }
+    cout << "Sum of " << N << " elements is equal to " << sum << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
