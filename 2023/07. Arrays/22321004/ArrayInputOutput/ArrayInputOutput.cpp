a3926da8a3f407e84259d2409eaca6414ffa945b
Лора Михаилова <lora@alein.org>
2023-05-10 20:36:01 +0300

    done

--- 2023/07. Arrays/22321004/ArrayInputOutput/ArrayInputOutput.cpp
+++ 2023/07. Arrays/22321004/ArrayInputOutput/ArrayInputOutput.cpp
@@ -1,20 +1,19 @@
-// ArrayInputOutput.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int N = 10;
+
+    int A[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << " ";
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
