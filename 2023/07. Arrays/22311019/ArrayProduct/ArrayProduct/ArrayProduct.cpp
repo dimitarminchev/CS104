ebb642497d9dc8fe86dbeffcc3497c728dc31dae
BorisHristov04 <borishristov04@gmail.com>
2023-05-10 18:33:50 +0300

    Done

--- 2023/07. Arrays/22311019/ArrayProduct/ArrayProduct/ArrayProduct.cpp
+++ 2023/07. Arrays/22311019/ArrayProduct/ArrayProduct/ArrayProduct.cpp
@@ -1,20 +1,22 @@
-// ArrayProduct.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
+#include<iostream>
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int N = 100;
+
+    double A[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = i + 1;
+    }
+
+    double pro = 1;
+    for (int i = 0; i < N; i++)
+    {
+        pro *= A[i];
+    }
+    cout << "Product of " << N << " elements is equal to " << pro << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
