6f6c8732ebc70d7e10cdaadd89975a2bb4110e99
BorisHristov04 <borishristov04@gmail.com>
2023-05-10 18:31:52 +0300

    Done

--- 2023/07. Arrays/22311019/ArrayBubbleSort/ArrayBubbleSort/ArrayBubbleSort.cpp
+++ 2023/07. Arrays/22311019/ArrayBubbleSort/ArrayBubbleSort/ArrayBubbleSort.cpp
@@ -1,20 +1,52 @@
-// ArrayBubbleSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include<cstdlib>
+using namespace std;
+
+const int N = 10;
+
+int A[N];
+
+void Print()
+{
+    for (int i = 0; i < N; i++)
+    {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+}
+
+void BubbleSort()
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (A[i] < A[j])
+            {
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand((unsigned)time(NULL));
+
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = rand();
+    }
+
+    cout << "Random generated " << N << " numbers:\n";
+    Print();
+
+    BubbleSort();
+
+    cout << "\nSorted numbers:\n";
+    Print();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
