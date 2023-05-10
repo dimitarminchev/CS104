d639345e4c2394674f4c8fe32d82f55241d1a674
Mehmed Nedjatin <teryeriblack@gmail.com>
2023-05-10 17:32:12 +0300

    Done

--- 2022/03. Maths/19321038/2. SumOdd/2. SumOdd.cpp
+++ 2022/03. Maths/19321038/2. SumOdd/2. SumOdd.cpp
@@ -3,12 +3,11 @@ using namespace std;
 int main()
 {
     int a;
-    cout << "Enter five digit number: ";
     cin >> a;
     int a1 = (a / 10000) % 10;
     int a3 = (a / 100) % 10;
     int a5 = (a / 1) % 10;
-    int sum = a1 + a3 + a5;
-    cout << "Sum: " << sum << endl;
+    cout << a1, a3, a5;
+
     return 0;
 }
