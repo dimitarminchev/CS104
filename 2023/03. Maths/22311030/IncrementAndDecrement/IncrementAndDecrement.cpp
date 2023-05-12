a13c470ea35392467d7c2ce4ac90350e5a66f03d
yarolavpy <yaroslav@deineka.net>
2023-05-12 17:21:21 +0300

    Done 	new file:   2023/03. Maths/22311030/IncrementAndDecrement/IncrementAndDecrement.cpp     modified:   .DS_Store 	modified:   2022/.DS_Store 	modified:   2023/02. FirstSteps/.DS_Store 	modified:   2023/03. Maths/22311030/IncrementAndDecrement/IncrementAndDecrement.cpp 	deleted:    2023/03. Maths/22311030/IncrementAndDecrement/IncrementAndDecrement.hpp 	2023/03. Maths/22311030/MathOperations/ 	2023/03. Maths/22311030/ProductOfNumberDigits/ 	2023/03. Maths/22311030/SquareEquation/ 	2023/03. Maths/22311030/SumOfNumberDigits/ 	2023/03. Maths/22311030/SurfaceAndAreaOfCircle/ 	2023/03. Maths/22311030/SurfaceAndAreaOfRectangle/ 	2023/04. Conditions/22311030/ 	2023/05. Cycles/22311030/ 	2023/06. Routines/22311030/ 	2023/07. Arrays/22311030/ 	2023/08. Matrixes/22311030/

---
+++ 2023/03. Maths/22311030/IncrementAndDecrement/IncrementAndDecrement.cpp
@@ -0,0 +1,8 @@
+//
+//  IncrementAndDecrement.cpp
+//  
+//
+//  Created by Yaro Deineka on 8.05.23.
+//
+
+#include "IncrementAndDecrement.hpp"
---
+++ 2023/03. Maths/22311030/IncrementAndDecrement/IncrementAndDecrement.hpp
@@ -0,0 +1,13 @@
+//
+//  IncrementAndDecrement.hpp
+//  
+//
+//  Created by Yaro Deineka on 8.05.23.
+//
+
+#ifndef IncrementAndDecrement_hpp
+#define IncrementAndDecrement_hpp
+
+#include <stdio.h>
+
+#endif /* IncrementAndDecrement_hpp */
