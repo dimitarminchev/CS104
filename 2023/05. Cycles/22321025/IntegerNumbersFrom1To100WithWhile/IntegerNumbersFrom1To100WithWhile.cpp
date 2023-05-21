35b81e3d87c16b9500fc530b8249a448f34340c2
Христо Христов <icoo0_96@mail.bg>
2023-05-21 18:42:33 +0300

    22321025

---
+++ 2023/05. Cycles/22321025/IntegerNumbersFrom1To100WithWhile/IntegerNumbersFrom1To100WithWhile.cpp
@@ -0,0 +1,15 @@
+﻿﻿#include <iostream>
+using namespace std;
+int main()
+{
+    // Отпечатване на числата от 1 до 100
+    int k = 1;
+
+    while (k <= 100)
+    {
+        cout << k << " ";
+        k = k + 1;
+    }
+
+    return 0;
+}
