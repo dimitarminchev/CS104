c6dd42c0ce88083acfa2250effd8bb17cc334da1
Dimita Minchev <dimitar.minchev@gmail.com>
2023-07-04 16:46:32 +0300

    Fixes

---
+++ 2023/03. Maths/22311025/22311025 incrementanddecrement/22311025 incrementanddecrement.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int X = 1, Y = 1;
+//увеличаване
+	cout << X + 1 << endl;
+	cout << X++ << endl;
+	cout << ++Y << endl;
+
+	// намаляне
+	cout << X - 1 << endl;
+	cout << X-- << endl;
+	cout << --Y << endl;
+	return 0;
+}
