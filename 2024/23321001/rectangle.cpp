46b43bc217675ec885878815f6d78e3d84c81181
metodialeksov <metodii_alexov@abv.bg>
2024-03-26 18:22:27 +0200

    yooooooo

---
+++ 2024/23321001/rectangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+    int main() 
+{
+    int a, b, c;
+    cout << "Enter a=?" << endl;
+    cin >> a;
+
+    cout << "Enter b=?" << endl;
+    cin >> b;
+
+    c = a*b;
+    cout << "Area = " << c;
+}
