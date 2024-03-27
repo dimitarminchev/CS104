ebb938d94de7f9de64114e2fbe7b93a62cfe00b6
Mi6a4e <mihail.stoyanov123@gmail.com>
2024-03-27 16:15:55 +0200

    7

--- 2024/23321019/7.square_equation2/7.square_equation2.cpp
+++ 2024/23321019/7.square_equation2/7.square_equation2.cpp
@@ -1,17 +1,31 @@
 ﻿//Подобрение решението на квадратното уравнение
 #include<iostream>
+#include<cmath>
 using namespace std;
+
 int main()
 {
-	int a, b, c;
-	cin >> a >> b >> c;
+    float a, b, c;
+    cout << "Enter the coefficients (a, b, and c) of the quadratic equation: ";
+    cin >> a >> b >> c;
 
-	float d = pow(b, 2) + 4 * a * c;
-	float x1 = ((-b) + sqrt(d)) / (2 * a);
-	float x2 = ((-b) - sqrt(d)) / (2 * a);
+    float d = b * b - 4 * a * c;
 
-	cout << "d=" << d << endl;
-	cout << "x1=" << x1 << endl;
-	cout << "x2=" << x2 << endl;
-	return 0;
+    if (d > 0) {
+        float x1 = (-b + sqrt(d)) / (2 * a);
+        float x2 = (-b - sqrt(d)) / (2 * a);
+        cout << "The quadratic equation has two real and distinct roots:" << endl;
+        cout << "x1 = " << x1 << endl;
+        cout << "x2 = " << x2 << endl;
+    }
+    else if (d == 0) {
+        float x = -b / (2 * a);
+        cout << "The quadratic equation has one real root:" << endl;
+        cout << "x = " << x << endl;
+    }
+    else {
+        cout << "The quadratic equation has no real roots." << endl;
+    }
+
+    return 0;
 }
