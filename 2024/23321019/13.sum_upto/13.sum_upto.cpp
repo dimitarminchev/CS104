bfd49fd0991745660a3ac11592a3a02b60fea3b3
Mi6a4e <mihail.stoyanov123@gmail.com>
2024-03-27 17:02:14 +0200

    13

---
+++ 2024/23321019/13.sum_upto/13.sum_upto.cpp
@@ -0,0 +1,19 @@
+﻿// Сумата на естествените числа до зададено число
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "enter a number: ";
+    cin >> n;
+
+    int sum = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        sum += i; // Добавяме текущото число към сумата
+    }
+
+    cout << "Sum of natural numbers to " << n << " е: " << sum << endl;
+
+    return 0;
+}
