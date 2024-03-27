263f3464b07af170c672beb796b97a26b62cbbc4
lilihristova <lilihristova85@gmail.com>
2024-03-27 17:13:18 +0200

    for

---
+++ 2024/23321054/for/for.cpp
@@ -0,0 +1,14 @@
+﻿// for.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	for (int i = 1; i <= 100; i++)
+	{
+		cout << i << " ";
+	}
+	cout << endl;
+	return 0;
+}
+
