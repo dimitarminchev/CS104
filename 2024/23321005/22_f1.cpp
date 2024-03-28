83814880aca3386f5cfaa9de6d43351ff4e8b409
GabrielSt1 <gabrielstanev27@gmail.com>
2024-03-28 16:31:53 +0200

    LALA

--- 2024/23321005/22_f1.cpp
+++ 2024/23321005/22_f1.cpp
@@ -3,15 +3,12 @@
 using namespace std;
 int main()
 {
-{
-    int x,n;
+    int x,n,sum=0;
     cin >> x >> n;
-    
-    for (int k = 1; k <= n; k++)
+    for (int k=1; k<= n; k++)
     {
-        float An = 1.0 / k;
-        cout << An << endl;
-    
+        sum += pow(x,k);
     }
+    cout << sum << endl;
     return 0;
 }
