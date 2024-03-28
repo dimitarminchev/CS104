c8d6962189deb0706b51878ad7e4b9edef738d53
radko94 <rapostolov94@gmail.com>
2024-03-28 12:33:51 +0200

    primes cleanup

--- 2024/23321023/18_primes.cpp
+++ 2024/23321023/18_primes.cpp
@@ -4,36 +4,10 @@ using namespace std;
 
 int main()
 {
-    unsigned int primesUpTo, divider = 2;
+    unsigned int primesUpTo;
 
     cout << "Up to which numbers we want to print numbers, sir?" << endl;
     cin >> primesUpTo;
-
-    // while (divider <= primesUpTo / 2)
-    // {
-
-    //     if (primesUpTo % divider == 0)
-    //     {
-    //         cout << divider << " ";
-    //     }
-
-    //     divider += 1;
-
-    // }
-
-    // for (unsigned int i = 2; i < primesUpTo; i++)
-    // {
-    //     unsigned int x[i];
-
-    //     for (unsigned int y = 0; y < primesUpTo / 2; i++)
-    //     {
-    //         if (i % y == 0)
-    //         {
-    //             cout << i << " ";
-    //         }
-    //     }
-
-    // }
 
     unsigned int x;
     unsigned int y;
