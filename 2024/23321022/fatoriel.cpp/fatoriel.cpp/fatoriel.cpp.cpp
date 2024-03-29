12141504cedc175902fa184fd5cdeb9c0822370e
ivaskova <ivanavaskova123@gmail.com>
2024-03-29 11:59:14 +0200

    fatoriel.cpp

--- 2024/23321022/fatoriel.cpp/fatoriel.cpp/fatoriel.cpp.cpp
+++ 2024/23321022/fatoriel.cpp/fatoriel.cpp/fatoriel.cpp.cpp
@@ -1,4 +1,4 @@
-// 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// fatoriel.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
