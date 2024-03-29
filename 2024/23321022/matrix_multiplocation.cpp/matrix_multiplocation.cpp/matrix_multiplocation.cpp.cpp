802bdce55fe3878ded31832a2b37dba8f3bb1f18
ivaskova <ivanavaskova123@gmail.com>
2024-03-29 15:50:20 +0200

    matrix_sum.cpp

--- 2024/23321022/matrix_multiplocation.cpp/matrix_multiplocation.cpp/matrix_multiplocation.cpp.cpp
+++ 2024/23321022/matrix_multiplocation.cpp/matrix_multiplocation.cpp/matrix_multiplocation.cpp.cpp
@@ -1,20 +1,18 @@
-// matrix_multiplocation.cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    int A[3][3], B[3][3], N;
+    for (int x = 0; x < 3; x++) for (int y = 0; y < 3; y++) cin >> A[x][y];
+    cin >> N;
+    for (int x = 0; x < 3; x++)
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            B[x][y] = A[x][y] * N;
+            cout << B[x][y] << '\t';
+        }
+        cout << '\n';
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
