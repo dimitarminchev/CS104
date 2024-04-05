9b501c36358ed3f3e2f85808a5c6460cef547d31
janetteangel <janette.systems.acc@gmail.com>
2024-04-05 11:16:37 +0300

    Update all

--- 2024/23321013/28_bubblesort.cpp
+++ 2024/23321013/28_bubblesort.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    const int len = 5;  // Фиксирана дължина на масива
+int main()
+ {
+    const int len = 5;  
     int array[len];
 
-    // Въвеждане на елементите на масива
     cout << "Въведете " << len << " числа: ";
-    for (int i = 0; i < len; i++) {
+    for (int i = 0; i < len; i++) 
+    {
         cin >> array[i];
     }
 
-    //Сортиране по метода на мехурчето
     for (int i = 0; i < len - 1; i++) {
         for (int j = 0; j < len - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                //  Разменяне на елементите
+            if (array[j] > array[j + 1]) 
+            {
                 int temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
