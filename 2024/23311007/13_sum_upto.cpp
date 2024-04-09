4796adac6c9931f4fa1b484642c27b566ba3e0fb
Alie Ali <alieali2023@icloud.com>
2024-04-09 14:29:01 +0200

    oki

--- 2024/23311007/13_sum_upto.cpp
+++ 2024/23311007/13_sum_upto.cpp
@@ -1,16 +1,20 @@
-// Цикъл с постусловие
+// 13
 #include<iostream>
 #include<cmath>
 using namespace std;
-int main()
+int main() 
 {
-    int i = 1;
-    do
+    int n;
+    cin >> n;
+
+    int sum = 0;
+
+    for(int k=1; k<=n; k++)
     {
-        cout  << i << " ";
-        i = i + 1;
-    } while (i <= 100);
-    cout << endl;
+        sum = sum + k;
+    }
+
+    cout << sum << endl;
+
     return 0;
-    
 }
