320d8a3cd5f4f4e5cee943167e1047cb7178f076
Boyan Kalkandjiev <bobkatadj@gmail.com>
2024-04-10 12:27:18 +0300

    3, 4, 5, 6

--- 2024/23311013/03_rectangle/03_rectangle.cpp
+++ 2024/23311013/03_rectangle/03_rectangle.cpp
@@ -1,7 +1,4 @@
-﻿// 02_sum.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 #define print(x) std::cout<<x<<std::endl;
 
