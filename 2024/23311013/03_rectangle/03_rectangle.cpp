9190da71f2dc2b47ad2799148e510b92a45e35d6
Boyan Kalkandjiev <bobkatadj@gmail.com>
2024-04-10 12:23:57 +0300

    3, 4, 5, 6 exercise

---
+++ 2024/23311013/03_rectangle/03_rectangle.cpp
@@ -0,0 +1,21 @@
+﻿// 02_sum.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+#define print(x) std::cout<<x<<std::endl;
+
+int main()
+{
+	int a,b;
+	
+	print("Enter a value of a: ");
+	cin >> a;
+	print("Enter a value of b: ");
+	cin >> b;
+
+	print("P = " << (a + b) * 2);
+	print("S = " << a * b);
+
+	return 0;
+}
---
+++ 2024/23311013/04_circle/04_circle.cpp
@@ -0,0 +1,20 @@
+﻿// 02_sum.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+#define print(x) std::cout<<x<<std::endl
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+int main()
+{
+	int r;
+
+	print("Enter a value of radius: ");
+	cin >> r;
+
+	print("P = " << 2 * M_PI * r);
+	print("S = " << M_PI * r * r);
+	print("V = " << (4 * M_PI * r * r * r) / 3);
+}
