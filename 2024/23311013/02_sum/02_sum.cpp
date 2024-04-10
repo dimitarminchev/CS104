5d97931d40faf8e0c7c37b15bfb7f5a7247806c4
Boyan Kalkandjiev <bobkatadj@gmail.com>
2024-04-10 10:25:04 +0300

    01, 02

---
+++ 2024/23311013/02_sum/02_sum.cpp
@@ -0,0 +1,21 @@
+﻿// 02_sum.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+#define print(x) std::cout<<x<<std::endl;
+
+int main()
+{
+    int a;
+    int b;
+
+    print("a=");
+    cin >> a;
+    print("b=");
+    cin >> b;
+
+    print("a+b=" << a + b);
+    return 0;
+}
+
