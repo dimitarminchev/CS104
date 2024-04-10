e5ab3cb042f29fb39291f1b610d3bcfdef4bbbf7
AtanasIV04 <naskoivanovnasko@gmail.com>
2024-04-10 11:54:53 +0300

    chaddimuss

--- 2024/23311005/04_circle/04_circle.cpp
+++ 2024/23311005/04_circle/04_circle.cpp
@@ -9,7 +9,7 @@ int main()
     float p = 2 * 3.14 * r;
     float s = 3.14 * r * r;
     float v = 4 * 3.14 * r * r;
-   
+
     cout << "p=" << p << endl;
     cout << "s=" << s << endl;
     cout << "v=" << v << endl;
