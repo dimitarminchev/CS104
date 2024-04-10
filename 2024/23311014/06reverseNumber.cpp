a78c26cce8ef874c040760787cb11ce880c167d8
Martin Tonchev <martintonchev77@gmail.com>
2024-04-10 12:45:12 +0300

    zad 6

--- 2024/23311014/06reverseNumber.cpp
+++ 2024/23311014/06reverseNumber.cpp
@@ -1,10 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-    int normal_num;
-    int rev_num;
+int main() {
+    int a;
+    cin >> a;
 
+    int a1 = (a/100)%10;
+    int a2 = (a/10)%10;
+    int a3 = (a/1)%10; 
 
+    cout << a3 << a2 << a1 << endl;
+    return 0;
 }
