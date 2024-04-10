815700726194f795c07ffc13fda034c4a6ce472e
Stivan Voyvodov <stivanvoivodov@gmail.com>
2024-04-10 10:48:04 +0300

    bla bla

---
+++ 2024/23311008/02_sum.cpp
@@ -0,0 +1,16 @@
+#include<lostream>
+using namespace std;
+int main()
+    
+    int a,b;
+
+    cout << "a=" ;
+    cin >> a;
+    cout << "b=" ;
+    cin >> b;
+
+    cout << "a+b=" << endl ;
+
+    return 0;
+
+    
