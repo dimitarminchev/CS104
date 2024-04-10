995ee5d2fe6ab02eee7683d55b32534d925b3fcc
GabrielSt1 <gabrielstanev27@gmail.com>
2024-04-10 11:49:03 +0300

    omg2

--- 2024/23311015/04 - Circle.cpp
+++ 2024/23311015/04 - Circle.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main()
 {
  float r;
-float pi = 3.14;
+ float pi = 3.14;
 
  cout << "r=";
  cin >> r;
 
-float p = 2 * pi * r;
-float s = pi * r * r;
-float v = (4 * pi * r * r * r) / 3;
+ float p = 2 * pi * r;
+ float s = pi * r * r;
+ float v = (4 * pi * r * r * r) / 3;
 
  cout << "p=" << p << endl;
  cout << "s=" << s << endl;
