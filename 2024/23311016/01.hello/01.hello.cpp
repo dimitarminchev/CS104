982ceb0732945ec6f034977a729d40b951b7e2ff
Vasilena Rusakova <vasilenavr@gmail.com>
2024-04-10 10:23:17 +0300

    hello

--- 2024/23311016/01.hello/01.hello.cpp
+++ 2024/23311016/01.hello/01.hello.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-    cout << "Hello World!"<<endl;
+    cout << "Hello World"<<endl;
     return 0;
 }
--- 2024/23311016/02.sum/02.sum.cpp
+++ 2024/23311016/02.sum/02.sum.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <cmath>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello Worldd!\n";
+	int a, b;
+
+	cout << "Enter num 1: ";
+	cin >> a;
+	cout << "Enter num 2: ";
+	cin >> b;
+	cout << a + b<< endl;
+
+	return 0;
 }
 
