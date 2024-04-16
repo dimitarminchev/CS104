dab1f5f531b472e607553a205f4bb8f592826daa
metodialeksov <metodii_aleksov@abv.bg>
2024-04-16 09:45:57 +0300

    changes

--- 2024/23311010/07sum.cpp
+++ 2024/23311010/07sum.cpp
@@ -5,10 +5,10 @@ int main()
     int a;
     cin >> a;
 
-    int a1 = (a/1000)%10;
-    int a2 = (a/100)%10;
-    int a3 = (a/10)%10;
-    int a4 = (a/1)%10;
+    int a4 = (a/1000)%10;
+    int a3 = (a/100)%10;
+    int a2 = (a/10)%10;
+    int a1 = (a/1)%10;
 
     cout << a4 + a3 + a2 + a1 << endl;
     cout << a4 * a3 * a2 * a1 << endl;
