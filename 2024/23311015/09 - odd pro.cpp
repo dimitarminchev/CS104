69a8dc1cf40b3e7b8e0cc4d0e82814ecfed6b5b5
GabrielSt1 <gabrielstanev27@gmail.com>
2024-04-16 09:46:14 +0300

    odd 8

--- 2024/23311015/09 - odd pro.cpp
+++ 2024/23311015/09 - odd pro.cpp
@@ -6,12 +6,12 @@ int main()
  cout << "Vuvedi 6 cifreno chislo = ";
  cin >> a;
 
- int a1 = (a/100000) % 10;
- int a2 = (a/10000) % 10;
- int a3 = (a/1000) % 10;
- int a4 = (a/100) % 10;
- int a5 = (a/10) % 10;
- int a6 = (a/1) % 10;
+ int a6 = (a/100000) % 10;
+ int a5 = (a/10000) % 10;
+ int a4 = (a/1000) % 10;
+ int a3 = (a/100) % 10;
+ int a2 = (a/10) % 10;
+ int a1 = (a/1) % 10;
 
  cout << "proizvedenie na chetni = " << a2 * a4 * a6 << endl;
 
