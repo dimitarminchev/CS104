a3dd3224ab78ecdeaec76fe2ef16144145df0b6f
Stela Gonzova <hebi1404@gmail.com>
2024-04-18 14:54:32 +0300

    17

--- 2024/23311009/16_circles.cpp
+++ 2024/23311009/16_circles.cpp
@@ -17,7 +17,7 @@ float d = sqrt(pow(x2-x1,2)+pow(y2-y1,2));
 // output 
 if(d> r1+r2) cout << "No common Points" <<endl;
 if(d= r1+r2) cout << "1 common Points" <<endl;
-if(d> r1+r2) cout << "2 common Points" <<endl;
+if(d< r1+r2) cout << "2 common Points" <<endl;
 
 
 
