a0bf95d9e768ecbf6e00554b3e709d17bc8f25a4
AtanasIV04 <naskoivanovnasko@gmail.com>
2024-04-18 16:03:43 +0300

    Цък ЦЪК

---
+++ 2024/23311016/22.sumNums/22.sumNums.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Please enter n: ";
+	cin >> n;
+
+	int sum = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		sum = i + sum;
+	}
+
+	cout << "The sum is: " << sum;
+	return 0;
+}
