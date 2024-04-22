a722bc6c383740e10afcd0f8abf5d238d6937c0f
Boyan Kalkandjiev <bobkatadj@gmail.com>
2024-04-22 12:40:52 +0300

    35-42

---
+++ 2024/23311013/41_countNums/41_countNums.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+#define print(x) std::cout<<x<<std::endl;
+
+int main()
+{
+    int n, x, count = 0;
+
+    print("n: ");
+    cin >> n;
+    print("x: ");
+    cin >> x;
+
+    int a[100];
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        if (a[i] == x) count++;
+    }
+
+    print(x << " repeats " << count << " times")
+
+    return 0;
+}
