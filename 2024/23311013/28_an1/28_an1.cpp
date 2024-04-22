4588e169f039b63e35398e67fa81a0da19b936c1
Boyan Kalkandjiev <bobkatadj@gmail.com>
2024-04-22 09:36:02 +0300

    16-35

---
+++ 2024/23311013/28_an1/28_an1.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+#define print(x) std::cout<<x<<std::endl;
+
+int main()
+
+{
+    int k;
+    print("Enter a number: ");
+    cin >> k;
+
+    for (int i = 1; i <= k; ++i)
+    {
+        float d = 1.0 / i;
+        print("1/" << i << " = " << d);
+    }
+
+    return 0;
+}
---
+++ 2024/23311013/33_refunfak/33_refunfak.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+#define print(x) std::cout<<x<<std::endl;
+
+double fak(int n)
+
+{
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    else {
+        return n * fak(n - 1);
+    }
+}
+
+int main()
+{
+    int n;
+
+    print("Enter max number: ");
+    cin >> n;
+    print(n << "! = " << fak(n));
+
+    return 0;
+}
