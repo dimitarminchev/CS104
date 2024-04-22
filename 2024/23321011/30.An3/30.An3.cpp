f9842c651c88fd013eee00c1ca2a959378c68650
Alexrangelov0474 <alex.iu.rangelov@gmail.com>
2024-04-22 17:05:18 +0300

    dont click

--- 2024/23321011/30.An3/30.An3.cpp
+++ 2024/23321011/30.An3/30.An3.cpp
@@ -1,20 +1,20 @@
-// 30.An3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cmath>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n;
+	float result = 0;
+	cout << "Please enter n: ";
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		result = pow(1.0 + (1.0 / i), i);
+		cout << result << " ";
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
