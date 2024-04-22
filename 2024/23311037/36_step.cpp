56b5b419ca317e3a6b07c5f5b662f5137e8546da
Berberian77 <gzdravko434@gmail.com>
2024-04-22 10:38:33 +0300

    huh

--- 2024/23311037/36_step.cpp
+++ 2024/23311037/36_step.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-
 using namespace std;
+
 int power(int a, int n)
+
 {
-    if(n == 1) return 2;
-    return 2 * power(n - 1);
+    if(n == 1) return a;
+    return a * power(a - 1);
 }
+
 int main()
+
 {
-    int n;
-    cout << "Enter n: " << endl;
-    cin >> n;
-    cout << "pow(2,n)" << power(n) << endl;
+    int n, a;
+    cout << "Enter a, n: " << endl;
+    cin >> a >> n;
+    cout << a << " ^ " << n << "= " << power(a, n) <<
     return 0;
 }
