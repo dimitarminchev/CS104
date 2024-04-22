4a38049affce09a6eb501e7c61d5d6fe9eae9407
Berberian77 <gzdravko434@gmail.com>
2024-04-22 12:42:26 +0300

    ЦАУУТ

--- 2024/23311037/41_counting.cpp
+++ 2024/23311037/41_counting.cpp
@@ -13,7 +13,7 @@ int main()
     for (int i = 0; i < n; i++ )
     {
 
-        cin >> a{i};
+        cin >> a[i];
         if(a[i] == x) count;
     }
     cout << x << " repeats " << count << "times. " << endl;
