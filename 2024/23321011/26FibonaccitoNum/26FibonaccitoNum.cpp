ff9365754d078173973c794251c019fa72b0cbdb
Alexrangelov0474 <alex.iu.rangelov@gmail.com>
2024-04-22 16:57:45 +0300

    dont click

--- 2024/23321011/26FibonaccitoNum/26FibonaccitoNum.cpp
+++ 2024/23321011/26FibonaccitoNum/26FibonaccitoNum.cpp
@@ -1,20 +1,41 @@
-// 26FibonaccitoNum.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n;
+	cout << "Please enter n: ";
+	cin >> n;
+
+	int a = 1;
+	int b = 1;
+	int fib = a + b;
+
+	if (n == 1)
+	{
+		cout << a << endl;
+	}
+	else if (n == 2)
+	{
+		cout << a << " " << b << endl;
+	}
+	else if (n == 2)
+	{
+		cout << a << " " << b << " " << fib << endl;
+	}
+	else
+
+	{
+		cout << a << " " << b << " " << fib << " ";
+		for (int i = 3; i < n; i++)
+		{
+			a = b;
+			b = fib;
+			fib = a + b;
+			cout << fib << " ";
+		}
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
