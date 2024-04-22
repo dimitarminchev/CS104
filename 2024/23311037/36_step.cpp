9ed1c4b1be89764b91e55ea630d1616508857d74
Berberian77 <gzdravko434@gmail.com>
2024-04-22 10:30:50 +0300

    y

--- 2024/23311037/36_step.cpp
+++ 2024/23311037/36_step.cpp
@@ -1,1 +1,16 @@
-// ДА си препиша задачатаs
+#include <iostream>
+
+using namespace std;
+int power(int a, int n)
+{
+    if(n == 1) return 2;
+    return 2 * power(n - 1);
+}
+int main()
+{
+    int n;
+    cout << "Enter n: " << endl;
+    cin >> n;
+    cout << "pow(2,n)" << power(n) << endl;
+    return 0;
+}
