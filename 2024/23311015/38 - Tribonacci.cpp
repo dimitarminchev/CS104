d7d4324f73cb7e0cee04a1ccb3297f48963b07bf
GabrielSt1 <gabrielstanev27@gmail.com>
2024-04-22 10:46:36 +0300

    trib

--- 2024/23311015/38 - Tribonacci.cpp
+++ 2024/23311015/38 - Tribonacci.cpp
@@ -6,10 +6,10 @@ int main()
     cout << "Enter n: " << endl;
     cin >> n;
 
-    int fib[100] = {0,1,1};
-    for (int i = 3; i < n; i++)
+    int fib[100] = {0,1,1,2};
+    for (int i = 4; i < n; i++)
     {
-        fib[i] = fib[i-1] + fib[i-2];
+        fib[i] = fib[i-1] + fib[i-2] + fib[i-3];
     }
 
     for (int i = 0; i < n; i++)
