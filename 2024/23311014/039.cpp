b2022db469bfe0f0d608388219ee29bccc81aaf6
Martin Tonchev <martintonchev77@gmail.com>
2024-04-22 11:50:43 +0300

    040,039

--- 2024/23311014/039.cpp
+++ 2024/23311014/039.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include <#cmath>
+#include <cmath>
+
+
 using namespace std;
 
 int main()
--- 2024/23311014/040.cpp
+++ 2024/23311014/040.cpp
@@ -3,30 +3,25 @@ using namespace std;
 
 int main()
 {
-    int arr[100] = {1,-2,3,5,-7,12};
-    int arrL = 6;
-    int max;
-    int min;
+    // how much is the fish?
+    int n;
+    cin >> n;
+    int min = 1000;
+    int max = -1000;
+
+    int a[100];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    
 
 
-    for(int i = 0;i < arrL; i++){
-        cout << arr[i] << endl;
-        // check for min
-        for(int k = 0;k < arrL;k++){    
-            if(arr[i] > arr[k]){
-                break;
-            }
-            min = arr[i];
-        }
-
-        for(int d = 0;d < arrL;d++){    
-            if(arr[i] < arr[d]){
-                break;
-            }
-            max = arr[i];
-        }
-
-
+    for(int i = 0;i < n; i++)
+    {
+        if(a[i] < min) min = a[i];
+        if(a[i] > max) max = a[i];
     }
 
 
