3706373844735474eae8d6185a99f99873d5604c
SnezhanaIvanova <sashaivanovska1@gmail.com>
2024-04-24 12:16:46 +0300

    new

--- 2024/23311026/46.matrix.sum/46.matrix.sum.cpp
+++ 2024/23311026/46.matrix.sum/46.matrix.sum.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 int main()
 {
-    int n;
     int a[3][3], b[3][3];
+
     cout << "Enter the matrix values: " << endl;
+
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
             cin >> a[i][j];
+
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
             cin >> b[i][j];
