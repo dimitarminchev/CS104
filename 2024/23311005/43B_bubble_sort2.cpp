813dd658cac5cd4c7c82a266ddb01480b1d1aeec
AtanasIV04 <naskoivanovnasko@gmail.com>
2024-04-24 10:57:39 +0300

    boom

--- 2024/23311005/43B_bubble_sort2.cpp
+++ 2024/23311005/43B_bubble_sort2.cpp
@@ -24,5 +24,6 @@ int main()
         
         
     }               
+    
     return 0;     
 }
---
+++ 2024/23311005/44_matrix_number.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int matrix[3][3]
+    cout <<"Enter matrix values: "<< endl;
+}
