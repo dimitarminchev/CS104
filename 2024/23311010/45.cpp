3c79df7ca851d555c8ef2ebadeff5b12ec833582
metodialeksov <metodii_aleksov@abv.bg>
2024-04-24 11:28:10 +0300

    the cost of preparedness - measured in gold, later in blood

---
+++ 2024/23311010/45.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "n: " << endl;
+    cin >> n;
+    int matrixA[3][3];
+    int matrixB[3][3];
+    cout << "Enter matrix values: " << endl;
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++) cin >> matrix[i][j];
+        cin >> matrixA[i][j]
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            cin >> matrixB[i][j];
+
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 3; j++)
+    cout << a[i][j] + b[i][j]<< "\t";
+    cout << endl;
+}
+return 0;
+}
