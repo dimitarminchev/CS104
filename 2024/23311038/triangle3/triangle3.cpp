e1b5d18715d8b713a953b0e3b796530c94b6278e
Kris4e <civilniq@gmail.com>
2024-05-01 21:58:59 +0300

    done3

---
+++ 2024/23311038/triangle3/triangle3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, p;
+    cout << "a=";
+    cin >> a;
+    cout << "b=";
+    cin >> b;
+    p = a * b;
+    cout << p << endl;
+    return 0;
+
+}
