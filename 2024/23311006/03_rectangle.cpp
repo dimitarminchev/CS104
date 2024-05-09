dee5daf98008e6d0c6a7958f5e22a6bd0f8770a2
Hrisi T <94146462+hrisitetsuq23@users.noreply.github.com>
2024-05-09 23:19:40 +0300

    Create 03_rectangle.cpp

---
+++ 2024/23311006/03_rectangle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+
+    cout << "a = " ;
+    cin >> a;
+
+    cout << "b = " ;
+    cin >> b;
+    int p = 2 * (a+b) ; 
+    int s = a * b; 
+    
+
+    cout << "p=" << a+b << endl;
+    cout << "s=" << a+b << endl;
+
+    return 0;
+}
