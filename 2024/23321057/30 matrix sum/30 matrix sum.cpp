f36df4691738c014af5d022009ba391994aec412
Веселина Славова <vess_enita@abv.bg>
2024-05-22 22:04:04 +0300

    omg

---
+++ 2024/23321057/30 matrix sum/30 matrix sum.cpp
@@ -0,0 +1,19 @@
+// 30 matrix sum
+#include<iostream>
+using namespace std;
+int main()
+{
+	int A[3][3], B[3][3], C[3][3], N;
+	for (int x = 0; x < 3; x++) for (int y = 0; y < 3; y++) cin >> A[x][y];
+	for (int x = 0; x < 3; x++) for (int y = 0; y < 3; y++) cin >> B[x][y];
+	for (int x = 0; x < 3; x++)
+	{
+		for (int y = 0; y < 3; y++)
+		{
+			C[x][y] = A[x][y] + B[x][y];
+			cout << C[x][y] << '\t';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
