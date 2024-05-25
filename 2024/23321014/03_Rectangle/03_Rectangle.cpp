9a61c08e47aa583ebe2a2da62e55d675e290b973
Radoslav354 <165040714+Radoslav354@users.noreply.github.com>
2024-05-25 22:40:51 +0300

    Add files via upload

---
+++ 2024/23321014/03_Rectangle/03_Rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sideA, sideB, p;
+    cout << "side A= ";
+    cin >> sideA;
+
+    cout << "side B= ";
+    cin >> sideB;
+
+    p = sideA * sideB;
+    cout << p << endl;
+
+    return 0;
+}
