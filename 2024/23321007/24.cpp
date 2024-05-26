be49359ec7ef679ac936274abc20c71901e21160
Dzeni Dimitriva <Dimitrivadzeni@gmail.com>
2024-05-26 21:55:01 +0300

    finishing with homework

---
+++ 2024/23321007/24.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int fak(int n)
+{
+	if (n == 1) return 1;
+	else return n * fak(n - 1);
+}
+
+int main()
+{
+	int n;
+	cout << "n="; cin >> n;
+	cout << "n!=" << fak(n) << endl;
+	return 0;
+}
