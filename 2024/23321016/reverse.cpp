2a45ebca08b5d1396ac43890ec0eb45c0b0f5199
NIkolayrr <nikolay.r.rangelov@gmail.com>
2024-05-27 09:40:49 +0300

    added exercise solutions for 23321016

---
+++ 2024/23321016/reverse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    int a1 = (num / 100) % 10;
+    int a2 = (num / 10) % 10;
+    int a3 = (num / 1) % 10;
+
+    cout << a3 << a2 << a1 << endl;
+    return 0;
+}
