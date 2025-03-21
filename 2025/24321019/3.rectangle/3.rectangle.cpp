3f53abff87bf623089dae2e2d39a36cb3525376f
nedjatirashid <necatirashid@gmail.com>
2025-03-21 16:58:32 +0200

    zad 3

---
+++ 2025/24321019/3.rectangle/3.rectangle.cpp
@@ -0,0 +1,21 @@
+//
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int a, b;
+    cout << "a=";
+    cin >> a;
+
+    cout << "b=";
+    cin >> b;
+    int c = a * b;
+        int d = 2 * (a + b);
+        cout << "P=" << d << endl;
+
+        cout << "S=" << c << endl;
+
+        return 0;
+}
