df10b1316a1b40dc276fe35a6bc60b87ae9a3c52
Ростислав Георгиев <24321010@students.bfu.bg>
2025-03-21 17:15:21 +0200

    Rectangle

---
+++ 2025/24321010/Rectangle/Rectangle.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "a=";
+	cin >> a;
+
+	cout << "b=";
+	cin >> b;
+
+	int p = 2 * ( a + b);
+	cout << "p = 2 * (a + b)" << p << endl;
+	
+	int S = a * b;
+	cout << "a * b " << S << endl;
+}
+
+
