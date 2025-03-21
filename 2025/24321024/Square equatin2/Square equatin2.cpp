790c311951caee703d3a164571056535529378f6
Nik NIkolov <Varkola@abv.bg>
2025-03-21 18:11:52 +0200

    MNOGOOO QKOOOO

--- 2025/24321024/Square equatin2/Square equatin2.cpp
+++ 2025/24321024/Square equatin2/Square equatin2.cpp
@@ -16,19 +16,25 @@ if (d < 0)
 
 }
 if (d == 0)
+
 {
 	cout << "d = 0, x1 = x2 = " << (-b) / (2 * a) << endl;
 
+
+
+
+
+	if (d > 0)
+
+		float d = pow(b, 2) - 4 * a * c;
+	float x1 = ((-b) + sqrt(d)) / (2 * a);
+	float x1 = ((-b) - sqrt(d)) / (2 * a);
+
+	cout << "d=" << d << endl;
+	cout << "x1=" << x1 << endl;
+	cout << "x2=" << x2 << endl;
+
+	return 0;
+
 }
 
-if (d > 0)
-
-float d = pow(b, 2) - 4 * a * c;
-float x1 = ((-b) + sqrt(d)) / (2 * a);
-float x1 = ((-b) - sqrt(d)) / (2 * a);
-
-cout << "d=" << d << endl;
-cout << "x1=" << x1 << endl;
-cout << "x2=" << x2 << endl;
-
-return 0;
