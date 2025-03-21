1f374e8e243018812b2a748c407f2053f2b7d744
MartinBainov <martinbainov@abv.bg>
2025-03-21 17:20:49 +0200

    Rectangle

--- 2025/24321007/Sum/Sum.cpp
+++ 2025/24321007/Sum/Sum.cpp
@@ -7,13 +7,17 @@ int main()
         int a;
         int b;
 
-        cout << "a= ";
+        cout << "Enter A side of the rectangle: ";
         cin >> a;
 
-        cout << "b= ";
+        cout << "Enter A side of the rectangle: ";
         cin >> b;
 
-        int c = a + b;
+        cout << "The perimeter of the rectangle is :"
+        cout << int c = 2*(a+b);
+
+        cout << "The face of the rectangle is :"
+        cout int d = a*b;
 
         cout << "a+b= " << c << endl;
         return 0;
