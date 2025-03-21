d660d05d5e6a32064bdb92a816113ce897777ad3
fatme ahmed <fatme7330@gmail.com>
2025-03-21 17:23:22 +0200

    :)

--- 2025/24321006/Circle/Circle.cpp
+++ 2025/24321006/Circle/Circle.cpp
@@ -2,12 +2,6 @@
 using namespace std;
 int main()
 {
-    int a = 7, b = 12;
-
-    int p = 2 * (a + b)
-        int s = a * b;
-
-    cout << "p=" << p << endl;
-    cout << "s=" << s << endl;
-    return 0;
+   
+    
 }
