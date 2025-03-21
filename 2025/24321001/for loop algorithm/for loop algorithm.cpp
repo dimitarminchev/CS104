900634886073f27ce9a055081b14fee2ff8b9cc4
pavel-05 <pavelkruchmarov010905@gmail.com>
2025-03-21 18:04:03 +0200

    while loop algorithm

--- 2025/24321001/for loop algorithm/for loop algorithm.cpp
+++ 2025/24321001/for loop algorithm/for loop algorithm.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 int main()
 {
+	// Извеждане на числата от 1 до 1000
 	for (int k = 1; k <= 1000; k++)
 	{
 		cout << "k = " << k << endl;
