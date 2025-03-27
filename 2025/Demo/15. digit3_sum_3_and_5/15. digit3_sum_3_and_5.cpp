8a2361bd3c059d19ac54d9bc0fb04bd7616ccfd6
Dimitar Minchev <dimitar.minchev@gmail.com>
2025-03-27 14:53:15 +0200

    актуализация

---
+++ 2025/Demo/15. digit3_sum_3_and_5/15. digit3_sum_3_and_5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    // Създаваме променлива за сума
+    int sum = 0;
+
+    // Цикъл който да намери сумата
+    for (int k = 100; k <= 999; k++)
+    {
+        // if(k % 3 ==0 && k % 5 == 0)
+        if (k % 15 == 0)
+        {
+            sum = sum + k;
+            cout << k << " ";
+        }
+    }
+
+    // Отпечатваме получената сума
+    cout << endl << sum << endl;
+
+    return 0;
+}
