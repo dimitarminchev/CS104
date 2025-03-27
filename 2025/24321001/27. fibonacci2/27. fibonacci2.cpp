c14aef9a5a3bf7050dbf041357cb9e3ca58a76ef
pavel-05 <pavelkruchmarov010905@gmail.com>
2025-03-27 17:36:20 +0200

    bubble sort

--- 2025/24321001/27. fibonacci2/27. fibonacci2.cpp
+++ 2025/24321001/27. fibonacci2/27. fibonacci2.cpp
@@ -2,17 +2,9 @@
 #include <cmath>
 using namespace std;
 
-// Рекурсивна функция за намиране степен на двойката
-int step2(int n)
-{
-	if (n == 1)	return 2;
-	else return 2 * step2(n - 1);
-}
 
-// Главна функция
 int main()
 {
-
 	// Създаваме масив с 10 елемента, и първите два са единици
 	int fibArr[10] = { 1,1 };
 
@@ -27,5 +19,4 @@ int main()
 	{
 		printf("%d ", fibArr[k]);
 	}
-
 }
