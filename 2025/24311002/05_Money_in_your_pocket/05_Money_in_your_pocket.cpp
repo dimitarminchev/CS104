4fdd1a2a035d625482589648e58f1c7125aa2fa1
YumiA38 <yumiakifova@gmail.com>
2025-04-02 15:44:31 +0300

    zadacha5

--- 2025/24311002/05_Money_in_your_pocket/05_Money_in_your_pocket.cpp
+++ 2025/24311002/05_Money_in_your_pocket/05_Money_in_your_pocket.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int m05,m10,m20,m50,m1,m2;
+    float m05,m10,m20,m50,m1,m2;
     cout << "Enter coins: 05, 10, 20, 50, 100 and 200" << endl;
     cin >> m05 >> m10 >> m20 >> m50 >> m1 >> m2;
 
