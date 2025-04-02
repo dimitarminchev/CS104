31db837dc701c5b91fc3c0b3324d608dcc74507e
Dimitar Minchev <dimitar.minchev@gmail.com>
2025-04-02 14:33:12 +0300

    zad.2

---
+++ 2025/Redovno/02_Suma/02_Suma.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream> 
+using namespace std;
+int main()
+{
+    // Деклариране на две целочислени променливи
+    int a,b;
+
+    // Молим потребителя да въведе а
+    cout << "a=";
+    cin >> a;
+
+    // Молим потребителя да въведе b
+    cout << "b=";
+    cin >> b;
+
+    // Как да намерим сумата?
+    int c = a + b;
+    cout << "a+b=" << c << endl;
+
+    return 0;
+}
