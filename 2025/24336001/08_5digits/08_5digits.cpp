b635ab1e2fafc6b3aebfd952bd6c7f48f7d77fa2
vanyatkostova <vanyakostova8@gmail.com>
2025-04-03 10:24:07 +0300

    Сума на цифрите на нечетни поз-ции на 5цифрено

---
+++ 2025/24336001/08_5digits/08_5digits.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Please, enter 5 digit number:" << endl;
+    cin >> n;
+
+    int a = (n / 10000) % 10;
+    int b = (n / 100) % 10;
+    int c = (n / 1) % 10;
+  
+
+    int S = a + b + c;
+   
+    cout << S << endl;
+
+    return 0;
+}
