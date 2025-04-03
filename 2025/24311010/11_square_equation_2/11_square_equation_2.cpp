c0686f458959fddbd9ceff81288a03f15292c597
TodorPopov04 <toshko.popov.1234@gmail.com>
2025-04-03 11:37:32 +0300

    Подобрена версия на квадратното уравнение

---
+++ 2025/24311010/11_square_equation_2/11_square_equation_2.cpp
@@ -0,0 +1,35 @@
+﻿// Подобрена версия на квадратно уравнение
+#include <iostream> // cout, cin
+#include <cmath> // pow, sqrt
+using namespace std;
+int main()
+{
+    // Коефициенти на квадратното уравнение
+    int a, b, c;
+
+    // Извеждаме подканващ текст и въвеждаме числата
+    cout << "Please enter square equation coeficients: a b c" << endl;
+    cin >> a >> b >> c;
+
+    // Изчисляваме дискриминанта
+    float d = pow(b, 2) - (4 * a * c);
+
+    // Решение
+    if (d < 0)
+    {
+        cout << "d < 0" << endl << "No solution" << endl;
+    }
+    if (d == 0)
+    {
+        cout << "d=0, x1=x2" << endl << "x=" << (-b) / (2 * a) << endl;
+    }
+    if (d > 0)
+    {
+        float x1 = ((-b) + sqrt(d)) / (2 * a);
+        float x2 = ((-b) - sqrt(d)) / (2 * a);
+        cout << "d=" << d << endl;
+        cout << "x1=" << x1 << endl;
+        cout << "x2=" << x2 << endl;
+    }
+    return 0;
+}
