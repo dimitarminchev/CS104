6824ae1ba0976190d2ec4ce03e6265126c55cb7a
vanyatkostova <vanyakostova8@gmail.com>
2025-04-03 10:27:44 +0300

    Произведние на цифрите на четни позиции на 6цифрено

---
+++ 2025/24336001/09_6digits/09_6digits.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Please, enter 6 digit number:" << endl;
+    cin >> n;
+
+    int a = (n / 100000) % 10;
+    int b = (n / 1000) % 10;
+    int c = (n / 10) % 10;
+
+
+    int p = a * b * c;
+
+    cout << p << endl;
+
+    return 0;
+}
