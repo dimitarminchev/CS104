50706d7786a9118be1eeeddcac7c289e8380dc0d
MysticalWanderer <nastya.tueva2005@gmail.com>
2025-04-10 11:22:34 +0300

    Suma na vsichki tricifreni kratni na 3 i 5

---
+++ 2025/24311003/24_3digitsSum/24_3digitsSum.cpp
@@ -0,0 +1,20 @@
+//Suma na vsichki tricifreni kratni na 3 i 5
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sum = 0;
+
+    for (int k = 100; k <= 999; k++)
+    {
+        if (k % 15 == 0)
+        {
+            sum = sum + k;
+        }
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
+
