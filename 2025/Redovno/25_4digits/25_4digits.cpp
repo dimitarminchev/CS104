7be100381c1f0c2ed07af66c53db969fca22e5a3
Dimitar Minchev <dimitar.minchev@gmail.com>
2025-04-10 11:48:44 +0300

    Отпечатайте тези четирицифрени числа, при които сбора на цифрите на десетици и единици е равен на сбора цифрите на хилядни и стотици

--- 2025/Redovno/25_4digits/25_4digits.cpp
+++ 2025/Redovno/25_4digits/25_4digits.cpp
@@ -1,13 +1,16 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 int main()
 {
+	// Четирицифрените числа
 	for (int k = 1000; k <= 9999; k++)
 	{
+		// Отцепваме цифрите
 		int a = (k / 1000) % 10; 
 		int b = (k / 100) % 10; 
 		int c = (k / 10) % 10; 
 		int d = (k / 1) % 10;
+		// Проверка 
 		if (a + b == c + d)
 		{
 			cout << k << " ";
