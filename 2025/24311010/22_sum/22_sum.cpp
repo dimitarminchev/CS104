cf397a608a2fe2e33cf395462dfed3b4bac8d3f5
TodorPopov04 <toshko.popov.1234@gmail.com>
2025-04-10 10:46:38 +0300

    Сумата на естествените числа до зададено число

---
+++ 2025/24311010/22_sum/22_sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, sum = 0;
+
+    cout << "n=";
+    cin >> n;
+
+    for (int k = 1; k <= n; k++)
+    {
+        sum = sum + k;
+    }
+
+    cout << "sum=" << sum << endl;
+
+    return 0;
+}
+
