c624c08ed3d77c39ff0eb6ce9ca430477bc121ac
MysticalWanderer <nastya.tueva2005@gmail.com>
2025-04-10 10:32:53 +0300

    Igra Click

---
+++ 2025/24311003/21_Click/21_Click.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int k = 1; k < 100; k++)
+    {
+        int a = k / 10;
+        int b = k % 10;
+        if (a == 7 || b == 7 || k % 7 == 0)
+        {
+            cout << "Click ";
+        }
+        else
+        {
+            cout << k << " ";
+        }
+    }
+    return 0;
+}
