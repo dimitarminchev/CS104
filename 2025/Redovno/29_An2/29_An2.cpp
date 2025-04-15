6b3b3a0d7eb755fa21b1f2c20e0d73856d662cff
Dimitar Minchev <dimitar.minchev@gmail.com>
2025-04-15 14:32:54 +0300

    Изведете редицата: An = 1 / (n^2)

---
+++ 2025/Redovno/29_An2/29_An2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    // Декларираме променлива и въвеждаме нейната стоност от клавиатурата
+    int n;
+    cout << "n=";
+    cin >> n;
+
+    // Изчисляваме редицата An
+    for (float i = 1; i <= n; i++)
+    {
+        cout << (1.0 / pow(i,2)) << endl;
+    }
+
+    return 0;
+}
