d8e1ce8503f9b220f65574f51c6bf77c0b9895e3
esin6 <esinesin214@gmail.com>
2025-04-15 14:33:27 +0300

    Изведете редица: An=1/(n^2)

---
+++ 2025/28_An1/29_An2/29_An2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "n=";
+	cin >> n;
+	for (float i = 1; i <= n; i++)
+	{
+		cout << (1.0 / pow (i,2)) << endl;
+	}
+	return 0;
+}
