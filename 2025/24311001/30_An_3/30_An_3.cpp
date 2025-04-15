041f61959fd4413a83e9df43b3e1c3e395368eac
martinibeibi1636 <stanislav_bolnev@abv.bg>
2025-04-15 14:38:59 +0300

    Извежда An=(1+(1/n))^2

---
+++ 2025/24311001/30_An_3/30_An_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "n=";
+	cin >> n;
+
+	for (float i = 1; i <= n; i++)
+	{
+		cout << pow((1+(1/i)),i) << endl;
+	}
+
+	return 0;
+}
