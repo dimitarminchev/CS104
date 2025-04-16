abb42afe7d33099c3b9ed5d32ef43be00d697dae
martinibeibi1636 <stanislav_bolnev@abv.bg>
2025-04-16 14:19:53 +0300

    Рекурсивна функция за намиране на k степен

--- 2025/24311001/36_Step_k/36_Step_k.cpp
+++ 2025/24311001/36_Step_k/36_Step_k.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
 using namespace std;
-int step2(int n)
+int step(int x, int n)
 {
-    if (n == 1) return 2;
-    else return 2 * step2(n - 1);
+    if (n == 1) return x;
+    else return x * step(x, n - 1);
 }
 int main()
 {
-    int n;
+    int x,n;
+    cout << "x=";
+    cin >> x;
     cout << "n=";
     cin >> n;
-    for (int k = 1; k <= n; k++)
-    {
-
-        cout << "2^" << k << "=" << step2(k) << endl;
-
-    }
-
-
+    cout << x << "^" << n << "=" << step(x, n) << endl;
     return 0;
 }
