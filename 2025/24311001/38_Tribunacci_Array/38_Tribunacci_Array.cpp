7e15d0d5a5f078b457a0eb958a5ac4916c01ee74
martinibeibi1636 <stanislav_bolnev@abv.bg>
2025-04-16 14:38:54 +0300

    Масив, решаващ Редицата на Трибоначи

--- 2025/24311001/38_Tribunacci_Array/38_Tribunacci_Array.cpp
+++ 2025/24311001/38_Tribunacci_Array/38_Tribunacci_Array.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main()
 {
     const int N = 50;
-    double fib[N] = { 1,1 };
+    double tri[N] = { 1,1 };
 
     for (int k = 2; k < N; k++)
     {
-        fib[k] = fib[k - 1] + fib[k - 2];
+        tri[k] = tri[k - 1] + tri[k - 2] + tri[k - 3];
     }
     for (int k = 0; k < N; k++)
-        cout << fib[k] << " ";
+        cout << tri[k] << " ";
     return 0;
 }
