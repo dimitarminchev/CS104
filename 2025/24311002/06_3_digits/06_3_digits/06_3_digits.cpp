851c852c1426826d557eaae41604304d625eb2dc
YumiA38 <yumiakifova@gmail.com>
2025-04-21 23:17:59 +0300

    Отцепване на цифрите на 3 цифрено число

---
+++ 2025/24311002/06_3_digits/06_3_digits/06_3_digits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int n;
+    cout << "Enter 3 digit number:" << endl;
+    cin >> n;
+
+    int a = (n / 100) % 10;
+    int b = (n / 10) % 10;
+    int c = (n / 1) % 10;
+
+    cout << c << b << a << endl;
+    return 0;
+}
