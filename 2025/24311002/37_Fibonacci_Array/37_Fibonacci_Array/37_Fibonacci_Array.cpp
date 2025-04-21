d29f0817f15b440fb63859f7091ea3781d894927
YumiA38 <yumiakifova@gmail.com>
2025-04-22 00:58:21 +0300

    Масив, решаващ Редицата на Фибоначи

---
+++ 2025/24311002/37_Fibonacci_Array/37_Fibonacci_Array/37_Fibonacci_Array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int N = 50;
+    double fib[N] = { 1,1 };
+
+    for (int k = 2; k < N; k++)
+    {
+        fib[k] = fib[k - 1] + fib[k - 2];
+    }
+    for (int k = 0; k < N; k++)
+        cout << fib[k] << " ";
+    return 0;
+}
