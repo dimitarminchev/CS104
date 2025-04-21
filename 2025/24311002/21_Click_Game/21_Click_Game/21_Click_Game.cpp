ba8eba02429c7ccb416516c4d70a2919c43765a2
YumiA38 <yumiakifova@gmail.com>
2025-04-22 00:37:37 +0300

    Играта "Click!"

---
+++ 2025/24311002/21_Click_Game/21_Click_Game/21_Click_Game.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int k;
+    for (k = 1; k < 100; k++)
+    {
+        int a = k / 10;
+        int b = k % 10;
+        if (a == 7 || b == 7 || k % 7 == 0)
+        {
+
+            cout << "Click! ";
+
+        }
+        else
+        {
+
+            cout << k << " ";
+
+
+        }
+
+    }
+    return 0;
+}
