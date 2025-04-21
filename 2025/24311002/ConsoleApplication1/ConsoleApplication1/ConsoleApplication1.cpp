d04e98f7b38207c8797a61da41a11fb8f6bf7535
YumiA38 <yumiakifova@gmail.com>
2025-04-22 00:21:23 +0300

    Изчисление на квадратно уравнение

---
+++ 2025/24311002/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "a= ";
+	cin >> a;
+	cout << "b= ";
+	cin >> b;
+	int p = (a + b) * 2;
+	cout << "Perimeter= " << p << endl;
+	return 0;
+
+}
+
