c5f61fae3b4e47463769eac561a16f813ae3e01e
YumiA38 <yumiakifova@gmail.com>
2025-04-22 00:51:34 +0300

    f= sum k [1...n] ((-1)^k)*(x^k)

---
+++ 2025/24311002/32_fSum_2/32_fSum_2/32_fSum_2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+
+
+	int x;
+	cout << "x=";
+	cin >> x;
+
+	int n;
+	cout << "n=";
+	cin >> n;
+
+	float sum = 0;
+	for (int k = 1; k <= n;k++)
+	{
+
+		sum += pow(-1, k) * pow(x, k);
+
+	}
+	cout << sum << endl;
+	return 0;
+}
