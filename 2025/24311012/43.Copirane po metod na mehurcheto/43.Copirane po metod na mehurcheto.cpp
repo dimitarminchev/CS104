21fab70be9b0b3b992b8b635ea5bf5911704a6a8
StaskichaUNI <stanislav_s05@abv.bg>
2025-05-13 14:24:16 +0300

    43

--- 2025/24311012/43.Copirane po metod na mehurcheto/43.Copirane po metod na mehurcheto.cpp
+++ 2025/24311012/43.Copirane po metod na mehurcheto/43.Copirane po metod na mehurcheto.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 int main()
 {
+	int A[10];
 
+	for (int i = 0; i < 10; i++) cin >> A[i];
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (A[i] < A[j])
+			{
+				int temp = A[i];
+				A[i] = A[j];
+				A[i] = temp;
+
+			}
+		}
+	}
+
+	for (int i = 0; i < 10; i++) cout << A[i] << " ";
+
+	return 0;
 }
