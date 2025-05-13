f827bab29c5d6643c98fb95c9a407d84b11ca113
dimitarminchev <dimitar.minchev@gmail.com>
2025-05-13 13:46:41 +0300

    Брой срещания на дадено число

---
+++ 2025/Redovno/42_Indexes/42_Indexes.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    // Деклариране на масив за цели числа
+    int A[10];
+
+    // Въвеждане на елементите на масива
+    for (int i=0; i < 10; i++) cin >> A[i];
+
+    // Число което търсим в масива
+    int b;
+    cout << "Search: ";
+    cin >> b;
+
+    // Търсене на числото в масива
+    cout << "Indexes: ";
+    for (int i = 0; i < 10; i++)
+    {
+        if (A[i] == b) cout << i << " ";
+    }
+
+    return 0;
+}
