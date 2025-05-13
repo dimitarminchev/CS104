a5bd762aa0477747ae2905412a2b8a8d7d3baccc
StaskichaUNI <stanislav_s05@abv.bg>
2025-05-13 14:06:01 +0300

    35

---
+++ 2025/24311012/35_step2/35_step2.cpp
@@ -0,0 +1,21 @@
+﻿﻿#include <iostream>
+using namespace std;
+// Рекурсивна функция за намиране на степен на двойката
+int step2(int n)
+{
+	if (n == 1) return 2;
+	else return 2 * step2(n - 1);
+}
+// Главна функция
+int main()
+{
+	int n;
+	cout << "n=";
+	cin >> n;
+
+	for (int k = 1; k <= n; k++)
+	{
+		cout << "2^" << k << "=" << step2(k) << endl;
+	}
+	return 0;
+}
