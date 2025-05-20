db49f44f0b5f2793abd87cd895ac9ecfb3e8ccbc
Teodora Todorova <just_theodora@abv.bg>
2025-05-20 15:51:08 +0300

    43

---
+++ 2025/24311011/43_bubblesort/43_bubblesort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A[10];
+
+    for (int i = 0; i < 10; i++) cin >> A[i];
+
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            if (A[i] < A[j])
+            {
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < 10; i++) cout << A[i] << " ";
+
+    return 0;
+}
