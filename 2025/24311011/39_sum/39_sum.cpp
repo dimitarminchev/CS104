56a1b02420a9e6c1c7baa7ded5e5c370ed216049
Teodora Todorova <just_theodora@abv.bg>
2025-05-20 15:45:54 +0300

    f = sum k=[1..n] x^k

---
+++ 2025/24311011/39_sum/39_sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int x, n, A[100], sum = 0;
+
+    cout << "x=";
+    cin >> x;
+    cout << "n=";
+    cin >> n;
+
+    for (int k = 0; k < n; k++)
+    {
+        A[k] = pow(x, k);
+        sum = sum + A[k];
+    }
+    cout << "sum=" << sum << endl;
+
+    return 0;
+}
