651e536008905df75d347d10f516831e031ec587
Teodora Todorova <just_theodora@abv.bg>
2025-05-20 15:32:51 +0300

    Изведете редицата: An = (1 + (1/n))^n

---
+++ 2025/24311011/30_An3/30_An3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "n=";
+    cin >> n;
+
+    for (float i = 1; i <= n; i++)
+    {
+        cout << pow((1.0 + (1.0 / i)), i) << endl;
+    }
+
+    return 0;
+}
