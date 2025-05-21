4892e1302eef4f96341e9067600d431e3beddd8e
mijirok <addgun53@gmail.com>
2025-05-21 19:49:32 +0300

    Монети в джоба

---
+++ 2025/24311020/05-coins/05-coins/05-coins.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+    int m05, m10, m20, m50, m1, m2;
+
+    cout << "Enter coins: 05, 10, 20, 50, 100 and 200" << endl;
+    cin >> m05 >> m10 >> m20 >> m50 >> m1 >> m2;
+    float sum = m05 * 0.05 + m10 * 0.1 + m20 * 0.2 + m50 * 0.5 + m1 * 1 + m2 * 2;
+
+    cout << "sum=" << sum << endl;
+
+    return 0;
+}
