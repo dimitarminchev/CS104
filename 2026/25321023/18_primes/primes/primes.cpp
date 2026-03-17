c3109fdca267d6cc215071824e508a43eb7187a1
zxxtlz <75642081+zxxtlz@users.noreply.github.com>
2026-03-17 17:23:11 +0200

    Pushing 2nd half, yes making, maker

---
+++ 2026/25321023/18_primes/primes/primes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 2; i <= n; i++) {
+        bool isPrime = true;
+
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
