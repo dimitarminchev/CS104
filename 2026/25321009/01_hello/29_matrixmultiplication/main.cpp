770320e578f8d23e74e95e6eca01183d33e1aaa0
25321009 <25321009@students.bfu.bg>
2026-03-18 14:55:25 +0200

    29 - matrix multiplication

---
+++ 2026/25321009/01_hello/29_matrixmultiplication/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int A[3][3], B[3][3], C[3][3];
+
+	cout << "A: " << endl;
+	for (int i=0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			cin >> A[i][j];
+
+	cout << "B: " << endl;
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			cin >> B[i][j];
+
+	// c = a + b
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			C[i][j] = 0;
+			for (int k = 0; k < 3; k++)
+				C[i][j] += A[i][k] * B[k][j];
+			cout << C[i][j] << '\t';
+		}
+		cout << endl;
+	}
+}
