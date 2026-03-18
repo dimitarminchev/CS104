28da48151b7cf25bff71ca93f8d16d2ad55bd4bf
Bojina <bojina.simeonova05@gmail.com>
2026-03-18 15:04:04 +0200

    Задачи

---
+++ 2026/25321011/26_step2/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int step2(int n)
+{
+	if (n == 1) return 2;
+	return 2 * step2(n - 1);
+}
+int main()
+{
+	int n;
+	cout << "n = ";
+	cin >> n;
+
+	cout << "Step 2 of " << n << " => " << step2(n) << endl;
+	return 0;
+}
