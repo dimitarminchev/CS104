724d6e01ab698c36b5d23fc094ed9ab0d49636b3
chernaev <v.chernaev@gmail.com>
2026-03-20 09:54:19 +0200

    Add files via upload

---
+++ 2026/18311038/17_fibonacci/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n, a = 1, b = 1, c = a + b;
+	cin >> n;
+	cout << "1 1 ";
+	while (c < n)
+	{
+		cout << c << " ";
+		a = b;
+		b = c;
+		c = a + b;
+	}
+	return 0;
+}
