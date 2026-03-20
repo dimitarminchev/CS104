96f9d8819ae0fb1e7796e943779846f87ad3171a
Alex <efremov.alex@yahoo.bg>
2026-03-20 16:44:27 +0200

    Задача №6

---
+++ 2026/25321013/06_square_equation/square_equation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	float a, b, c;
+	cout << "Enter values for a, b and c: ";
+	cin >> a >> b >> c;
+
+	float discriminant = pow(b, 2) - (4 * a * c);
+	float x1 = ((-b) + sqrt(discriminant)) / (2 * a);
+	float x2 = ((-b) - sqrt(discriminant)) / (2 * a);
+
+	cout << "discriminant: " << discriminant << endl;
+	cout << "x1: " << x1 << endl;
+	cout << "x2: " << x2 << endl;
+
+	return 0;
+}
