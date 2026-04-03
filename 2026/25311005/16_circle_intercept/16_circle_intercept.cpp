637cabad9758be71e20cbf5cb22c800b63c0f434
AniAleksandrova <25311005@students.bfu.bg>
2026-04-03 19:41:12 +0300

    16 16

---
+++ 2026/25311005/16_circle_intercept/16_circle_intercept.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	float Ax, Ay, Ar, Bx, By, Br;
+	cout << "Enter circle A (x, y, r,):" << endl;
+	cin >> Ax >> Ay >> Ar;
+	cout << "Enter circle B ( x, y, r):" << endl;
+	cin >> Bx >> By >> Br;
+
+	float d = sqrt(pow(Ax - Bx, 2) + pow(Ay - By, 2));
+
+	if (d > Ar + Br) cout << "Circles has No common point of intercept" << endl;
+	if (d == Ar + Br) cout << "Circles has ONE common point of intercept" << endl;
+	if (d < Ar + Br) cout << "Circles has TWO common points of intercept" << endl;
+
+	return 0;
+}
