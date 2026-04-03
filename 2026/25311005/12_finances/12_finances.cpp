ef75ebd4ae73e4a254f86395f35528cf7a037c62
AniAleksandrova <25311005@students.bfu.bg>
2026-04-03 18:21:36 +0300

    12fi

---
+++ 2026/25311005/12_finances/12_finances.cpp
@@ -0,0 +1,27 @@
+#include <iostream> // cout, cin
+#include <cmath> // pow, sqrt
+using namespace std;
+int main()
+{
+	float investment;
+	cout << "Please enter your investment" << endl;
+	cin >> investment;
+
+	int menu;
+	cout << "Bank menu:" << endl;
+	cout << "1( 2%" << endl;
+	cout << "2( 3%" << endl;
+	cout << "3( 4%" << endl;
+	cout << "4( 5%" << endl;
+	cout << "Choose: ";
+	cin >> menu;
+
+	float receive;
+	if (menu == 1) receive = investment + (investment * 0.02); // 2%
+	if (menu == 2) receive = investment + (investment * 0.03); // 3%
+	if (menu == 3) receive = investment + (investment * 0.04); // 4%
+	if (menu == 4) receive = investment + (investment * 0.05); // 5%
+	cout << "Receive: " << receive << endl;
+
+	return 0;
+}
