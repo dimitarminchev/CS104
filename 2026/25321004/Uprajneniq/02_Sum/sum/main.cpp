203b8eac0e51f6e68d00f12663c18189f6e7d259
mihaeladineva566 <mihaeladineva566@gmail.com>
2026-04-10 15:59:24 +0300

    Add files via upload

---
+++ 2026/25321004/Uprajneniq/02_Sum/sum/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+
+    cout << "Vuvedi purvo chislo: ";
+    cin >> a;
+
+    cout << "Vuvedi vtoro chislo: ";
+    cin >> b;
+
+    sum = a + b;
+
+    cout << "Sumata e: " << sum << endl;
+
+    cin.get();
+    cin.get();
+    return 0;
+}
