84022c1beafea19a4a7f7fe6a40a00e0dd499cf2
kirinouwu <pasha23012007@gmail.com>
2026-04-21 15:39:35 +0300

    faaaaah

---
+++ 2026/25311020/04_circle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r;
+    // radius number input
+    cout << "r=";
+    cin  >> r;
+
+    // math
+    float p = 2 * 3.14 * r;
+    float s = 3.14 * r * r;
+    float v = (4.0 / 3.0) * 3.14 * r * r * r;
+
+
+    // results
+    cout << "p=" << p << endl;
+    cout << "s=" << s << endl;
+    cout << "v=" << v << endl;
+
+    return 0;
+}
