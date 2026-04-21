70a583bc98e126d2e5e0e045892c2a68d428f86f
Paul456789 <smolakovpavel08@gmail.com>
2026-04-21 16:05:47 +0300

    DIscriminant calculation

---
+++ 2026/25311014/04_circle/04_circle.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	double R, pi = 3.14;
+
+	std::cout << "Enter radius:"; std::cin >> R;
+
+	std::cout << "Perimeter: " << 2 * pi * R << "\n"
+		<< "Square: " << pi * std::pow(R, 2) << "\n"
+		<< "Volume: " << 4 / 3 * pi * std::pow(R, 3);
+
+	return 0;
+}
---
+++ 2026/25311014/05_Money in the pocket/05_Money in the pocket.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	double sum = 0.0, amount[8]{};
+	int multiplier[8]{ 1, 2, 5, 10, 20, 50, 1, 2 };
+
+	std::cout << "Enter how many money you have:\n";
+
+	for (int i = 0; i <= 7; i++)
+	{
+		std::cin >> amount[i];
+
+		if (i == 7 || i == 8) { std::cout << " euro(s)"; }
+		else { std::cout << " euro cent(s)"; }
+	}
+
+	for (int i = 0; i <= 7; i++) { if (i < 7) { amount[i] /= 100; } sum += amount[i] * multiplier[i]; }
+
+	std::cout << "You have" << sum << "euro(s)";
+
+	system("pause");
+}
