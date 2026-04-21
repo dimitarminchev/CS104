4c445e2373cbdd628891d49dfcdafe3b182d4496
Dragomira06 <25311019@students.bfu.bg>
2026-04-21 15:01:05 +0300

    money money mnogo moneyyyyy

---
+++ 2026/25311019/05_pocket_money/05_pocket_money.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m01, m02, m05, m10, m20, m50, m1, m2;
+    cout << "Enter euro cents: 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1 & 2" << endl;
+    cin >> m01 >> m02 >> m05 >> m10 >> m20 >> m50 >> m1 >> m2;
+
+    float sum = m01 * 0.01 + m02 * 0.02 + m05 * 0.05 + m10 * 0.1 + m20 * 0.2 + m50 * 0.5 + m1 * 1 + m2 * 2;
+    cout << sum << "euro" << endl;
+    return 0;
+} 
