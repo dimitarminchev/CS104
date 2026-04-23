dc9bd6985d0fe6aea069b9a535e148b7b81dc570
alexemandiev-eng <alex.emandiev@icloud.com>
2026-04-23 10:50:22 +0300

    troina magiq

---
+++ 2026/25311003/15_sp/15_sp.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int menu;
+    cout << "Menu: " << endl;
+	cout << "1) Square" << endl;
+	cout << "2) Rectangle" << endl;
+	cout << "3) Circle" << endl;
+	cout << "Plese Select: " << endl;
+	cin >> menu;
+
+	if (menu == 1)
+	{
+		int a;	
+		cout << "a: " << endl;
+		cin >> a;
+		int s = a * a;
+		int p = 4 * a;
+		cout << "p=" << p << endl << "s=" << s << endl;
+	}
+
+	if (menu == 2)
+	{
+		int a, b;
+		cout << "a: ";
+		cin >> a;
+		cout << "b: ";
+		cin >> b;
+		int s = a * b;
+		int p = 2 * (a + b);
+		cout << "p=" << p << endl << "s=" << s << endl;
+	}
+
+	if (menu == 3)
+	{
+		float r;
+		cout << "r: ";
+		cin >> r;
+		int s = 3.14 * r * r;
+		int p = 2 * 3.14 * r;
+		cout << "p=" << p << endl << "s=" << s << endl;
+	}
+	return 0;
+}
+
+
