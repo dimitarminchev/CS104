e07f0d98d77d811dd6d9353daa3fa252cee7a64e
stkostova1 <25311013@students.bfu.bg>
2026-04-28 15:19:55 +0300

    13. Дължим данък към НАП за 2016

---
+++ 2026/25311013/13_nap2016/13_nap2016.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+#include <cmath> 
+using namespace std;
+int main()
+{
+    float income, tax = 0;
+    cout << "Please enter your income for 2016:" << endl;
+    cin >> income;
+
+    if (income <= 1720) tax = 0;
+    else if (income <= 3000) tax = (income - 1720) * 0.20;
+    else if (income <= 7200) tax = 120 + (income - 3000) * 0.22;
+    else  tax = 1072 + (income - 7200) * 0.24;
+
+    cout << "Tax = " << tax << endl;
+
+    return 0;
+
+}
