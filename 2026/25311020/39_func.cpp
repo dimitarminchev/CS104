a10cccbb697fb877cbf4c21ca33aa04e8c20ade6
kirinouwu <pasha23012007@gmail.com>
2026-05-14 10:31:01 +0300

    a

---
+++ 2026/25311020/39_func.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main () 
+{
+    const int n = 1000;
+    int f [n+1];
+    float sum = 0;
+
+    int N, x;
+    cout << "x=";
+    cin >> x;
+    co
+}
