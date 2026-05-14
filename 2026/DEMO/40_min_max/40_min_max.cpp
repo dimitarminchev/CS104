b4b404a2c14afd0146daff6a6ec74e4093060dd5
Dimitar Minchev <dimitar.minchev@gmail.com>
2026-05-14 11:09:04 +0300

    up to task 42

---
+++ 2026/DEMO/40_min_max/40_min_max.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    // Целочислен масив с 10 елемента
+    int A[10] = { 2,2,-1,-1,-1,3,5,-2,-2,4 };
+
+    // Променливи за минимум и максимум
+    int min = 1000, max = -1000;
+
+    // Цикъл за обхождане на елементите на масива
+    for (int k = 0; k < 10; k++)
+    {
+        if (A[k] < min) min = A[k]; 
+        if (A[k] > max) max = A[k];
+        cout << A[k] << " ";
+    }
+    cout << endl;
+
+    // Минимално и максимално число
+    cout << "Min: " << min << endl;
+    cout << "Max: " << max << endl;
+
+    return 0;
+}
