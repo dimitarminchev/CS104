b324fd344fa3f71c8658b4051d17df828b5862bc
alexemandiev-eng <alex.emandiev@icloud.com>
2026-05-14 09:26:29 +0300

    up to 38

---
+++ 2026/25311003/38_trib/38_trib.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	const int N = 15;
+    
+    int trib[10] = { 1, 1, 1 };
+    for (int k = 3; k < N; k++)
+    {
+        trib[k] = trib[k - 3] + trib[k - 2] + trib[k - 1];
+    }
+    for (int k = 0; k < N; k++)
+    {
+		cout << trib[k] << " ";
+    }
+}
