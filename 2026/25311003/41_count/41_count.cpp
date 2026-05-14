1f58e2358ec9aa925cf091866e0e95ff1c138042
alexemandiev-eng <alex.emandiev@icloud.com>
2026-05-14 10:32:29 +0300

    up to 41

---
+++ 2026/25311003/41_count/41_count.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A[10] = { 2, 2, -1, -1, -1, 3, 5, -2, -2, 4 };
+    int counter = 0, x;
+	cout << "x=";
+    cin >> x;
+    for (int k = 0; k < 10; k++)
+    {
+        if (A[k] == x) counter++;
+        cout << A[k] << " ";
+    }
+    cout << endl;
+    cout << "Count:" << counter << endl;
+	return 0;
+}
+
