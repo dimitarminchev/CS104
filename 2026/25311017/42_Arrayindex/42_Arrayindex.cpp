467b3f4cb36ee50d09da111489986988c73e788d
mehmed00 <mehmedk.dev@gmail.com>
2026-05-15 10:33:14 +0300

    42

---
+++ 2026/25311017/42_Arrayindex/42_Arrayindex.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A[10] = { 2,2,-1,-1,-1,3,5,-2,-2,4 };
+    int x, none = -1;
+    for (int i = 0; i < 10; i++) cout << A[i] << " ";
+    cout << endl << "x=";
+    cin >> x;
+    cout << "Indexes: " << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        if (A[i] == x)
+        {
+            cout << i << endl;
+            none = i;
+        }
+    }
+    if (none == -1) cout << "None" << endl;
+    return 0;
+}
